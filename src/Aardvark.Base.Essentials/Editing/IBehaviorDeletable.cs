cfe461def418a8cdc5374ab61e56270da3f2c5d7
Stefan Maierhofer <sm@stefanmaierhofer.com>
2014-12-30 12:27:01 +0100

    refactoring and cleanup

---
+++ src/Aardvark.Base.Essentials/Editing/IBehaviorDeletable.cs
@@ -0,0 +1,13 @@
+﻿namespace Aardvark.Base
+{
+    /// <summary>
+    /// Editable object can be deleted.
+    /// </summary>
+    public interface IBehaviorDeletable : IBehavior
+    {
+        /// <summary>
+        /// Deletes editable object.
+        /// </summary>
+        void Delete();
+    }
+}
---
+++ src/Aardvark.Base.Essentials/Editing/IBehaviorSplittableEdge2d.cs
@@ -0,0 +1,13 @@
+﻿namespace Aardvark.Base
+{
+    /// <summary>
+    /// Editable edge that can be splitted.
+    /// </summary>
+    public interface IBehaviorSplittableEdge2d : IBehavior
+    {
+        /// <summary>
+        /// Splits edge at position nearest to given split position.
+        /// </summary>
+        void Split(V2d splitPosition);
+    }
+}
