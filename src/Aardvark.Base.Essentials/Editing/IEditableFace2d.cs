cd5f1c1a24b45ae420073b666f6f28623897be23
Stefan Maierhofer <sm@stefanmaierhofer.com>
2014-12-30 13:32:08 +0100

    Aardvark.Base.Essentials: added IEditable[Vertex,Edge,Face]2d and IEditableSequence

---
+++ src/Aardvark.Base.Essentials/Editing/IEditableFace2d.cs
@@ -0,0 +1,22 @@
+﻿namespace Aardvark.Base
+{
+    /// <summary>
+    /// An editable face.
+    /// </summary>
+    public interface IEditableFace2d :
+        IBehaviorPosition2d,
+        IBehaviorTransform2d,
+        IBehaviorDeletable,
+        IEditableSequence<IImmutablePolygon2d>
+    {
+        /// <summary>
+        /// Returns index-th vertex as editable vertex.
+        /// </summary>
+        IEditableVertex2d GetEditableVertex(int index);
+
+        /// <summary>
+        /// Returns index-th edge as editable edge.
+        /// </summary>
+        IEditableEdge2d GetEditableEdge(int index);
+    }
+}
---
+++ src/Aardvark.Base.Essentials/Editing/IEditableVertex2d.cs
@@ -0,0 +1,13 @@
+﻿namespace Aardvark.Base
+{
+    /// <summary>
+    /// An editable vertex.
+    /// </summary>
+    public interface IEditableVertex2d :
+        IBehaviorPosition2d,
+        IBehaviorTransform2d,
+        IBehaviorDeletable,
+        IEditableSequence<V2d>
+    {
+    }
+}
