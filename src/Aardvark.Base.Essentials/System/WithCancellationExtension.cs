a55ac3122a55306f585fa087cd080534cdf76b4b
Harald Steinlechner <hs@vrvis.at>
2015-01-28 09:44:33 +0100

    fixed warning as error in Aardvark.Base.Essentials

--- src/Aardvark.Base.Essentials/System/WithCancellationExtension.cs
+++ src/Aardvark.Base.Essentials/System/WithCancellationExtension.cs
@@ -39,8 +39,8 @@ namespace Aardvark.Base
                 return task.WithCancellationInternal(ct.Value);
         }
 
-        private static readonly Action<object> s_cancellationRegistration =
-            s => ((TaskCompletionSource<bool>)s).TrySetResult(true);
+        //private static readonly Action<object> s_cancellationRegistration =
+        //    s => ((TaskCompletionSource<bool>)s).TrySetResult(true);
 
         private static async Task<T> WithCancellationInternal<T>(this Task<T> task, CancellationToken ct)
         {
