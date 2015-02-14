4c6bd89ca39fc4d0d99861f7768966daabd6176b
krauthaufen <krauthaufen@awx.at>
2015-02-14 22:05:13 +0100

    moved DrawCallInfo to Base.Rendering;

---
+++ src/Aardvark.Base.Rendering/Types/DrawCallInfo.cs
@@ -0,0 +1,13 @@
+﻿namespace Aardvark.Base
+{
+    public struct DrawCallInfo
+    {
+        public IndexedGeometryMode Mode;
+
+        public int FirstIndex;
+        public int FaceVertexCount;
+
+        public int FirstInstance;
+        public int InstanceCount;
+    }
+}
