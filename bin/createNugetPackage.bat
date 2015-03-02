a5f941cea6a30646cd803625b256f33e8b7f523b
haaser <haaser@vrvis.at>
2015-03-02 13:08:14 +0100

    added Aardvark.Base.Incremental nuspec;

--- bin/createNugetPackage.bat
+++ bin/createNugetPackage.bat
@@ -8,3 +8,6 @@ nuget pack Aardvark.Base.Essentials.nuspec
 msbuild ../src/Aardvark.Base.TypeProviders/Aardvark.Base.TypeProviders.fsproj /t:Build /p:Configuration="Release"
 msbuild ../src/Aardvark.Base.FSharp/Aardvark.Base.FSharp.fsproj /t:Build /p:Configuration="Release"
 nuget pack Aardvark.Base.FSharp.nuspec
+
+msbuild ../src/Aardvark.Base.Incremental/Aardvark.Base.Incremental.fsproj /t:Build /p:Configuration="Release"
+nuget pack Aardvark.Base.Incremental.nuspec
