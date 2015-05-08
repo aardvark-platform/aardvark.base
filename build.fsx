eab441f4b98e01fa28fc7fd4d005931460e77943
Harald Steinlechner <hs@vrvis.at>
2015-05-08 16:17:32 +0200

    restore packages for automatic package creation

--- build.fsx
+++ build.fsx
@@ -11,13 +11,42 @@ let core = !!"src/**/*.fsproj" ++ "src/**/*.csproj" -- "src/**/CodeGenerator.csp
 
 printfn "%A" (core |> Seq.toList)
 
+let packageRx = System.Text.RegularExpressions.Regex @"(?<name>.*?)\.(?<version>([0-9]+\.)*[0-9]+)\.nupkg$"
+
+let updatePackages (sources : list<string>) (projectFiles : #seq<string>) =
+    let packages = 
+        sources |> List.collect (fun source ->  
+            Directory.GetFiles(source,"*.nupkg") 
+                |> Array.choose (fun s -> 
+                    let m = Path.GetFileName s |> packageRx.Match in if m.Success then m.Groups.["name"].Value |> Some else None
+                ) 
+                |> Array.toList
+        ) |> Set.ofList
+
+    if Set.count packages <> 0 then
+        for project in projectFiles do
+            project |> Fake.NuGet.Update.NugetUpdate (fun p ->  
+                { p with 
+                    Ids = packages |> Set.intersect (Set.ofList p.Ids) |> Set.toList
+                    //ToolPath = @"E:\Development\aardvark-2015\tools\NuGet\nuget.exe"
+                    RepositoryPath = "packages"
+                    Sources = sources @ Fake.NuGet.Install.NugetInstallDefaults.Sources
+                    //Prerelease = true
+                } 
+            )
+
 Target "Restore" (fun () ->
 
     let packageConfigs = !!"src/**/packages.config" |> Seq.toList
 
+    let addtionalSources = (environVarOrDefault "AdditionalNugetSources" "").Split(';') |> Array.toList
     let defaultNuGetSources = RestorePackageHelper.RestorePackageDefaults.Sources
     for pc in packageConfigs do
-        RestorePackage (fun p -> { p with OutputPath = "packages" }) pc
+        RestorePackage (fun p -> { p with OutputPath = "packages"
+                                          Sources = addtionalSources @ defaultNuGetSources  
+                                 }) pc
+
+    updatePackages addtionalSources  (!!"src/**/*.csproj" ++ "src/**/*.fsproj")
 
 
 )
