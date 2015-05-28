d2f212815caae097d11f116f8889e5b80b6d8765
haaser <haaser@vrvis.at>
2015-05-28 08:33:05 +0200

    sketched AdaptiveMap;

--- src/Aardvark.Base.Incremental/Core/Cache.fs
+++ src/Aardvark.Base.Incremental/Core/Cache.fs
@@ -75,3 +75,95 @@ type Cache<'a, 'b>(scope : Ag.Scope, f : 'a -> 'b) =
                         cache.Remove v |> ignore
                     r
                 | _ -> failwithf "cannot revoke unknown value: %A" v
+
+
+
+/// <summary>
+/// Cache represents a cached function which can be 
+/// invoked and revoked. invoke increments the reference
+/// count for a specific argument (possibly causing the 
+/// function to be executed) whereas revoke decreases the
+/// reference count and removes the cache entry whenever
+/// the reference count is 0.
+/// </summary>
+type Cache2<'a, 'b, 'c>(scope : Ag.Scope, f : 'a -> 'b -> 'c) =  
+    let cache = Dictionary<obj, 'c * ref<int>>()
+    let mutable nullCache = None
+
+    /// <summary>
+    /// Clear removes all entries from the Cache and
+    /// executes a function for all removed cache entries.
+    /// this function is helpful if the contained values
+    /// are (for example) disposable resources.
+    /// </summary>
+    member x.Clear(remove : 'c -> unit) =
+        for (KeyValue(_,(v,_))) in cache do remove v
+        cache.Clear()
+        match nullCache with
+            | Some (v,_) -> 
+                remove v
+                nullCache <- None
+            | None -> ()
+
+    /// <summary>
+    /// invoke returns the function value associated
+    /// with the given argument (possibly executing the function)
+    /// and increases the associated reference count.
+    /// </summary>
+    member x.Invoke (a : 'a, b : 'b) =
+        let ao = a :> obj
+        let bo = b :> obj
+        if ao = null && bo = null then
+            match nullCache with
+                | Some (r, ref) -> 
+                    ref := !ref + 1
+                    r
+                | None ->
+                    let r = Ag.useScope scope (fun () -> f a b)
+                    nullCache <- Some(r, ref 1)
+                    r
+        else
+            let key =
+                if ao = null then bo
+                elif bo = null then ao
+                else (ao,bo) :> obj
+
+            match cache.TryGetValue key with
+                | (true, (r, ref)) -> 
+                    ref := !ref + 1
+                    r
+                | _ ->
+                    let r = Ag.useScope scope (fun () -> f a b)
+                    cache.[key] <- (r, ref 1)
+                    r
+
+    /// <summary>
+    /// revoke returns the function value associated
+    /// with the given argument and decreases its reference count.
+    /// </summary>
+    member x.Revoke (a : 'a, b : 'b) =
+        let ao = a :> obj
+        let bo = b :> obj
+        if ao = null && bo = null then
+            match nullCache with
+                | Some (r, ref) -> 
+                    ref := !ref - 1
+                    if !ref = 0 then
+                        nullCache <- None
+                    r
+                | None -> failwithf "cannot revoke null"
+        else
+            let key =
+                if ao = null then bo
+                elif bo = null then ao
+                else (ao,bo) :> obj
+
+            match cache.TryGetValue key with
+                | (true, (r, ref)) -> 
+                    ref := !ref - 1
+                    if !ref = 0 then
+                        cache.Remove key |> ignore
+                    r
+                | _ -> failwithf "cannot revoke unknown value: %A" key
+
+
---
+++ src/Aardvark.Base.Incremental/Tests/ASetPerformance.fs
@@ -0,0 +1,71 @@
+﻿namespace Aardvark.Base.Incremental.Tests
+
+open System
+open System.Collections
+open System.Collections.Generic
+open Aardvark.Base.Incremental
+open NUnit.Framework
+open FsUnit
+open System.Diagnostics
+
+module ``performance tests`` =
+
+    let timed name f =
+        let sw = Stopwatch()
+        sw.Start()
+        let r = f()
+        sw.Stop()
+        let str = sprintf "%s took: %.2fms" name sw.Elapsed.TotalMilliseconds
+        Console.WriteLine str
+        r
+
+    [<Test>]
+    let ``collect performance``() =
+        
+        let input = CSet.ofList []
+
+        let step (s : aset<'a>) =
+            s |> ASet.collect ASet.single
+
+        let rec stepN n s =
+            if n <= 0 then s
+            else stepN (n-1) (step s)
+            
+                
+
+        let test = stepN 200 input
+        let r = test.GetReader()
+
+        r.GetDelta() |> ignore
+
+
+        for i in 0..100 do
+            transact (fun () ->
+                input.UnionWith [i]
+            )
+            r.GetDelta() |> ignore
+
+        transact (fun () ->
+            input.Clear()
+        )
+        r.GetDelta() |> ignore
+
+
+        let changeTime = Stopwatch()
+        let evalTime = Stopwatch()
+
+        let iter = 1000
+        for i in 1..iter do
+            changeTime.Start()
+            transact (fun () ->
+                input.Add i |> ignore
+            )
+            changeTime.Stop()
+
+            evalTime.Start()
+            r.GetDelta() |> ignore
+            evalTime.Stop()
+
+        Console.WriteLine("change: {0}ms", changeTime.Elapsed.TotalMilliseconds / float iter )
+        Console.WriteLine("eval:   {0}ms", evalTime.Elapsed.TotalMilliseconds / float iter)
+        ()
