9f8419c8a5cf538723a83dfa811fac52b7c76f7e
krauthaufen <krauthaufen@awx.at>
2015-07-03 11:43:25 +0200

    added references/app.config for Aardvark.Base.FSharp.Tests;

---
+++ src/Tests/Aardvark.Base.FSharp.Tests/TimeTests.fs
@@ -0,0 +1,21 @@
+﻿namespace Aardvark.Base.FSharp.Tests
+
+open Aardvark.Base
+open FsUnit
+open NUnit.Framework
+
+module ``Time tests`` =
+    
+    [<Test>]
+    let ``indexing test``() =
+        
+        let r = Time.newRoot()
+        r.[0] |> should equal r
+
+        r |> Time.nth 1 |> should equal None
+        r |> Time.nth -1 |> should equal None
+
+
+
+
+
