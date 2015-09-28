ea4ff251578cd5172a2b40f00d9c1721391b4195
Harald Steinlechner <haraldsteinlechner@gmail.com>
2015-09-28 10:13:50 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
-( cd src;  mono NuGet.exe restore  -ConfigFile nuget.config)
+if [ ! -d "packages/FAKE" ]; then
+	echo "downloading FAKE"
+	mono --runtime=v4.0 bin/nuget.exe install FAKE -OutputDirectory packages -Version 3.35.2 -ExcludeVersion
+	mono --runtime=v4.0 bin/nuget.exe install Paket.Core -OutputDirectory packages -Version 1.18.5 -ExcludeVersion 
+fi
+
+
+mono packages/FAKE/tools/FAKE.exe "build.fsx"  $@
