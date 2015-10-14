db47b5ff0358a3feb19095c999ec446788349fda
Robert F. Tobler <rft@vrvis.at>
2015-10-14 12:07:13 +0200

    [Aardvark.Base] added hashcode computation for grids

--- src/Aardvark.Base/AlgoDat/HashCode.cs
+++ src/Aardvark.Base/AlgoDat/HashCode.cs
@@ -203,5 +203,280 @@ namespace Aardvark.Base
                            e3.Equals(default(T3)) ? 0 : e3.GetHashCode());
         }
 
+        /// <summary>
+        /// Compute the first of two possible hashcodes for hashing in a 1-D
+        /// unit grid. Add items with this function, retrieve with function
+        /// HashCode.Get2.
+        /// </summary>
+        public static int Get1of2(double x)
+        {
+            var xi = (long)System.Math.Floor(x);
+
+            return (int)(xi >> 1);
+        }
+
+        /// <summary>
+        /// Compute all two possible hashcodes for hashing in a 1-D unit grid.
+        /// Retrive all items with the two hashodes written into the supplied
+        /// array. Items need to be added just with the first of the two
+        /// hashcodes (also computed by function HashCodeGet1of2).
+        /// </summary>
+        public static void Get2(double x, int[] hca)
+        {
+            var xi = (long)System.Math.Floor(x);
+
+            int xh0 = (int)(xi >> 1);
+
+            hca[0] = xh0;
+            hca[1] = xh0 - 1 + ((int)(xi & 1) << 1);
+        }
+
+        /// <summary>
+        /// Compute the first of four possible hashcodes for hashing in a 2-D
+        /// unit grid. Add items with this function, retrieve with function
+        /// HashCode.Get4.
+        /// </summary>
+        public static int Get1of4(double x, double y)
+        {
+            var xi = (long)System.Math.Floor(x);
+            var yi = (long)System.Math.Floor(y);
+
+            return Combine((int)(xi >> 1), (int)(yi >> 1));
+        }
+
+        /// <summary>
+        /// Compute all four possible hashcodes for hashing in a 2-D unit grid.
+        /// Retrive all items with the four hashodes written into the supplied
+        /// array. Items need to be added just with the first of the four
+        /// hashcodes (also computed by function HashCodeGet1of4).
+        /// </summary>
+        public static void Get4(double x, double y, int[] hca)
+        {
+            var xi = (long)System.Math.Floor(x);
+            var yi = (long)System.Math.Floor(y);
+
+            int xh0 = (int)(xi >> 1), xh1 = xh0 - 1 + ((int)(xi & 1) << 1);
+            int yh0 = (int)(yi >> 1), yh1 = yh0 - 1 + ((int)(yi & 1) << 1);
+
+            hca[0] = Combine(xh0, yh0);
+            hca[1] = Combine(xh1, yh0);
+            hca[2] = Combine(xh0, yh1);
+            hca[3] = Combine(xh1, yh1);
+        }
+
+        /// <summary>
+        /// Compute the first of eight possible hashcodes for hashing in a 3-D
+        /// unit grid. Add items with this function, retrieve with function
+        /// HashCode.Get8.
+        /// </summary>
+        public static int Get1of8(double x, double y, double z)
+        {
+            var xi = (long)System.Math.Floor(x);
+            var yi = (long)System.Math.Floor(y);
+            var zi = (long)System.Math.Floor(z);
+
+            return Combine((int)(xi >> 1), (int)(yi >> 1), (int)(zi >> 1));
+        }
+
+        /// <summary>
+        /// Compute all eight possible hashcodes for hashing in a 3-D unit grid.
+        /// Retrive all items with the eight hashodes written into the supplied
+        /// array. Items need to be added just with the first of the eight
+        /// hashcodes (also computed by function HashCodeGet1of2).
+        /// </summary>
+        public static void Get8(double x, double y, double z, int[] hca)
+        {
+            var xi = (long)System.Math.Floor(x);
+            var yi = (long)System.Math.Floor(y);
+            var zi = (long)System.Math.Floor(z);
+
+            int xh0 = (int)(xi >> 1), xh1 = xh0 - 1 + ((int)(xi & 1) << 1);
+            int yh0 = (int)(yi >> 1), yh1 = yh0 - 1 + ((int)(yi & 1) << 1);
+            int zh0 = (int)(zi >> 1), zh1 = zh0 - 1 + ((int)(zi & 1) << 1);
+
+            hca[0] = Combine(xh0, yh0, zh0);
+            hca[1] = Combine(xh1, yh0, zh0);
+            hca[2] = Combine(xh0, yh1, zh0);
+            hca[3] = Combine(xh1, yh1, zh0);
+            hca[4] = Combine(xh0, yh0, zh1);
+            hca[5] = Combine(xh1, yh0, zh1);
+            hca[6] = Combine(xh0, yh1, zh1);
+            hca[7] = Combine(xh1, yh1, zh1);
+        }
+
+        /// <summary>
+        /// Compute the first of 16 possible hashcodes for hashing in a 4-D
+        /// unit grid. Add items with this function, retrieve with function
+        /// HashCode.Get16.
+        /// </summary>
+        public static int Get1of16(double x, double y, double z, double w)
+        {
+            var xi = (long)System.Math.Floor(x);
+            var yi = (long)System.Math.Floor(y);
+            var zi = (long)System.Math.Floor(z);
+            var wi = (long)System.Math.Floor(w);
+
+            return Combine((int)(xi >> 1), (int)(yi >> 1), (int)(zi >> 1), (int)(wi >> 1));
+        }
+
+        /// <summary>
+        /// Compute all 16 possible hashcodes for hashing in a 4-D unit grid.
+        /// Retrive all items with the 16 hashodes written into the supplied
+        /// array. Items need to be added just with the first of the 16
+        /// hashcodes (also computed by function HashCodeGet1of16).
+        /// </summary>
+        public static void Get16(double x, double y, double z, double w, int[] hca)
+        {
+            var xi = (long)System.Math.Floor(x);
+            var yi = (long)System.Math.Floor(y);
+            var zi = (long)System.Math.Floor(z);
+            var wi = (long)System.Math.Floor(w);
+
+            int xh0 = (int)(xi >> 1), xh1 = xh0 - 1 + ((int)(xi & 1) << 1);
+            int yh0 = (int)(yi >> 1), yh1 = yh0 - 1 + ((int)(yi & 1) << 1);
+            int zh0 = (int)(zi >> 1), zh1 = zh0 - 1 + ((int)(zi & 1) << 1);
+            int dh0 = (int)(wi >> 1), dh1 = dh0 - 1 + ((int)(wi & 1) << 1);
+
+            hca[0] = Combine(xh0, yh0, zh0, dh0);
+            hca[1] = Combine(xh1, yh0, zh0, dh0);
+            hca[2] = Combine(xh0, yh1, zh0, dh0);
+            hca[3] = Combine(xh1, yh1, zh0, dh0);
+            hca[4] = Combine(xh0, yh0, zh1, dh0);
+            hca[5] = Combine(xh1, yh0, zh1, dh0);
+            hca[6] = Combine(xh0, yh1, zh1, dh0);
+            hca[7] = Combine(xh1, yh1, zh1, dh0);
+            hca[8] = Combine(xh0, yh0, zh0, dh1);
+            hca[9] = Combine(xh1, yh0, zh0, dh1);
+            hca[10] = Combine(xh0, yh1, zh0, dh1);
+            hca[11] = Combine(xh1, yh1, zh0, dh1);
+            hca[12] = Combine(xh0, yh0, zh1, dh1);
+            hca[13] = Combine(xh1, yh0, zh1, dh1);
+            hca[14] = Combine(xh0, yh1, zh1, dh1);
+            hca[15] = Combine(xh1, yh1, zh1, dh1);
+        }
+
+        /// <summary>
+        /// Compute the first of four possible hashcodes for hashing in a 2-D
+        /// unit grid. Add items with this function, retrieve with function
+        /// HashCode.Get4.
+        /// </summary>
+        public static int Get1of4(V2d point)
+        {
+            var xi = (long)System.Math.Floor(point.X);
+            var yi = (long)System.Math.Floor(point.Y);
+
+            return Combine((int)(xi >> 1), (int)(yi >> 1));
+        }
+
+        /// <summary>
+        /// Compute all four possible hashcodes for hashing in a 2-D unit grid.
+        /// Retrive all items with the four hashodes written into the supplied
+        /// array. Items need to be added just with the first of the four
+        /// hashcodes (also computed by function HashCodeGet1of4).
+        /// </summary>
+        public static void Get4(V2d point, int[] hca)
+        {
+            var xi = (long)System.Math.Floor(point.X);
+            var yi = (long)System.Math.Floor(point.Y);
+
+            int xh0 = (int)(xi >> 1), xh1 = xh0 - 1 + ((int)(xi & 1) << 1);
+            int yh0 = (int)(yi >> 1), yh1 = yh0 - 1 + ((int)(yi & 1) << 1);
+
+            hca[0] = Combine(xh0, yh0);
+            hca[1] = Combine(xh1, yh0);
+            hca[2] = Combine(xh0, yh1);
+            hca[3] = Combine(xh1, yh1);
+        }
+
+        /// <summary>
+        /// Compute the first of eight possible hashcodes for hashing in a 3-D
+        /// unit grid. Add items with this function, retrieve with function
+        /// HashCode.Get8.
+        /// </summary>
+        public static int Get1of8(V3d point)
+        {
+            var xi = (long)System.Math.Floor(point.X);
+            var yi = (long)System.Math.Floor(point.Y);
+            var zi = (long)System.Math.Floor(point.Z);
+
+            return Combine((int)(xi >> 1), (int)(yi >> 1), (int)(zi >> 1));
+        }
+
+        /// <summary>
+        /// Compute all eight possible hashcodes for hashing in a 3-D unit grid.
+        /// Retrive all items with the eight hashodes written into the supplied
+        /// array. Items need to be added just with the first of the eight
+        /// hashcodes (also computed by function HashCodeGet1of2).
+        /// </summary>
+        public static void Get8(V3d point, int[] hca)
+        {
+            var xi = (long)System.Math.Floor(point.X);
+            var yi = (long)System.Math.Floor(point.Y);
+            var zi = (long)System.Math.Floor(point.Z);
+
+            int xh0 = (int)(xi >> 1), xh1 = xh0 - 1 + ((int)(xi & 1) << 1);
+            int yh0 = (int)(yi >> 1), yh1 = yh0 - 1 + ((int)(yi & 1) << 1);
+            int zh0 = (int)(zi >> 1), zh1 = zh0 - 1 + ((int)(zi & 1) << 1);
+
+            hca[0] = Combine(xh0, yh0, zh0);
+            hca[1] = Combine(xh1, yh0, zh0);
+            hca[2] = Combine(xh0, yh1, zh0);
+            hca[3] = Combine(xh1, yh1, zh0);
+            hca[4] = Combine(xh0, yh0, zh1);
+            hca[5] = Combine(xh1, yh0, zh1);
+            hca[6] = Combine(xh0, yh1, zh1);
+            hca[7] = Combine(xh1, yh1, zh1);
+        }
+
+        /// <summary>
+        /// Compute the first of 16 possible hashcodes for hashing in a 4-D
+        /// unit grid. Add items with this function, retrieve with function
+        /// HashCode.Get16.
+        /// </summary>
+        public static int Get1of16(V4d point)
+        {
+            var xi = (long)System.Math.Floor(point.X);
+            var yi = (long)System.Math.Floor(point.Y);
+            var zi = (long)System.Math.Floor(point.Z);
+            var wi = (long)System.Math.Floor(point.W);
+
+            return Combine((int)(xi >> 1), (int)(yi >> 1), (int)(zi >> 1), (int)(wi >> 1));
+        }
+
+        /// <summary>
+        /// Compute all 16 possible hashcodes for hashing in a 4-D unit grid.
+        /// Retrive all items with the 16 hashodes written into the supplied
+        /// array. Items need to be added just with the first of the 16
+        /// hashcodes (also computed by function HashCodeGet1of16).
+        /// </summary>
+        public static void Get16(V4d point, int[] hca)
+        {
+            var xi = (long)System.Math.Floor(point.X);
+            var yi = (long)System.Math.Floor(point.Y);
+            var zi = (long)System.Math.Floor(point.Z);
+            var wi = (long)System.Math.Floor(point.W);
+
+            int xh0 = (int)(xi >> 1), xh1 = xh0 - 1 + ((int)(xi & 1) << 1);
+            int yh0 = (int)(yi >> 1), yh1 = yh0 - 1 + ((int)(yi & 1) << 1);
+            int zh0 = (int)(zi >> 1), zh1 = zh0 - 1 + ((int)(zi & 1) << 1);
+            int dh0 = (int)(wi >> 1), dh1 = dh0 - 1 + ((int)(wi & 1) << 1);
+
+            hca[0] = Combine(xh0, yh0, zh0, dh0);
+            hca[1] = Combine(xh1, yh0, zh0, dh0);
+            hca[2] = Combine(xh0, yh1, zh0, dh0);
+            hca[3] = Combine(xh1, yh1, zh0, dh0);
+            hca[4] = Combine(xh0, yh0, zh1, dh0);
+            hca[5] = Combine(xh1, yh0, zh1, dh0);
+            hca[6] = Combine(xh0, yh1, zh1, dh0);
+            hca[7] = Combine(xh1, yh1, zh1, dh0);
+            hca[8] = Combine(xh0, yh0, zh0, dh1);
+            hca[9] = Combine(xh1, yh0, zh0, dh1);
+            hca[10] = Combine(xh0, yh1, zh0, dh1);
+            hca[11] = Combine(xh1, yh1, zh0, dh1);
+            hca[12] = Combine(xh0, yh0, zh1, dh1);
+            hca[13] = Combine(xh1, yh0, zh1, dh1);
+            hca[14] = Combine(xh0, yh1, zh1, dh1);
+            hca[15] = Combine(xh1, yh1, zh1, dh1);
+        }
     }
 }
