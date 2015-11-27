5ccdbe857d1f35c76b40d47cbc6c010746199a09
krauthaufen <krauthaufen@awx.at>
2015-11-27 15:36:34 +0100

    UndoRedo sketch;

--- src/Tests/Aardvark.Base.Incremental.Tests/Program.fs
+++ src/Tests/Aardvark.Base.Incremental.Tests/Program.fs
@@ -2,6 +2,6 @@
 
 [<EntryPoint>]
 let main args =
+    Aardvark.Base.Incremental.Tests.SimpleTest.run()
     //Aardvark.Base.Incremental.Tests.``collect tests``.``[ASet] async registerCallback``()
-    Aardvark.Base.Incremental.Tests.``collect tests``.``[ASet] memory leak test`` ()
     0
---
+++ src/Tests/Aardvark.Base.Incremental.Tests/UndoRedo.fs
@@ -0,0 +1,117 @@
+﻿namespace Aardvark.Base.Incremental.Tests
+
+open System
+open System.Collections
+open System.Collections.Generic
+open Aardvark.Base.Incremental
+open NUnit.Framework
+open FsUnit
+open System.Diagnostics
+open System.Linq
+
+type ISnapshot =
+    abstract member Restore : unit -> unit
+
+type ISnapshotThingy =
+    abstract member NewSnapshot : unit -> ISnapshot
+
+type Snapshot(inner : list<ISnapshot>) =
+    member x.Restore() =
+        for i in inner do i.Restore()
+
+    member x.Count = List.length inner
+
+
+    interface ISnapshot with
+        member x.Restore() = x.Restore()
+
+
+
+type UndoRedoScope() =
+    let dirty = HashSet<ISnapshotThingy>()
+
+    member x.Changed(o : ISnapshotThingy) =
+        lock dirty (fun () -> dirty.Add o |> ignore)
+
+    member x.Snapshot() =
+        let dirty =
+            lock dirty (fun () ->
+                let res = dirty.ToArray()
+                dirty.Clear()
+                res |> Array.toList
+            )
+
+        dirty
+            |> List.map (fun d -> d.NewSnapshot() )
+            |> Snapshot
+
+
+[<AutoOpen>]
+module ScopeExtensions =
+
+    type private UndoRedoModRef<'a>(scope : UndoRedoScope, value : 'a) =
+        inherit Mod.AbstractMod<'a>()
+
+        let mutable value = value
+
+        member x.Value
+            with get() = value
+            and set v =
+                value <- v
+                scope.Changed x
+                x.MarkOutdated()
+
+        interface ISnapshotThingy with
+            member x.NewSnapshot() =
+                let v = x.GetValue()
+                { new ISnapshot with member y.Restore() = x.Value <- v }
+    
+
+        interface IModRef<'a> with
+            member x.Value
+                with get() = value
+                and set v = x.Value <- v
+
+        override x.Compute() =
+            value
+
+
+    type UndoRedoScope with
+        member x.initMod (v : 'a) = 
+            let res = UndoRedoModRef(x, v)
+            x.Changed res
+            res :> IModRef<_>
+
+
+module SimpleTest =
+    
+    let run() =
+        let scope = UndoRedoScope()
+
+        let m = scope.initMod 10
+        let d = m |> Mod.map (fun a -> a * 2)
+        d |> Mod.force |> printfn "d = %A"
+
+
+        // create a snapshot
+        let s0 = scope.Snapshot()
+
+
+        // change the system a little
+        transact (fun () ->
+            Mod.change m 1000
+        )
+        d |> Mod.force |> printfn "d = %A"
+
+        // create a second snapshot
+        let s1 = scope.Snapshot()
+
+        // restore s0
+        transact (fun () -> s0.Restore())
+        d |> Mod.force |> printfn "s0: d = %A"
+        
+        // restore s1
+        transact (fun () -> s1.Restore())
+        d |> Mod.force |> printfn "s1: d = %A"
+
+
