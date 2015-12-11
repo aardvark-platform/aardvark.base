3adfe48bf1e93316cda47dd039e81de705ebecb7
Robert F. Tobler <rft@vrvis.at>
2015-12-11 12:02:21 +0100

    [Aaardvark.Base] added NullReporter (preparation for per thread silencing)

--- src/Aardvark.Base/IO/Report.cs
+++ src/Aardvark.Base/IO/Report.cs
@@ -915,6 +915,58 @@ namespace Aardvark.Base
         public void Dispose() { if (!Disposed)  Report.End(Level); }
     }
 
+    public class NullReporter : IJobReporter
+    {
+        public int Indent
+        {
+            get { return 0; }
+            set { }
+        }
+
+        public void AddReporter(IReporter reporter)
+        {
+        }
+
+        public ReportJob Begin(int level, ILogTarget target, string text, bool timed)
+        {
+            return null;
+        }
+
+        public double End(int level, ILogTarget target, string text)
+        {
+            return 0.0;
+        }
+
+        public void Line(LogType type, int level, ILogTarget target, string leftText, int rightPos = 0, string rightText = null)
+        {
+        }
+
+        public void Progress(int level, ILogTarget target, double progress, bool relative = false)
+        {
+        }
+
+        public void RemoveReporter(IReporter reporter)
+        {
+        }
+
+        public void Tests(TestInfo testInfo)
+        {
+        }
+
+        public void Text(int level, ILogTarget target, string text)
+        {
+        }
+
+        public void Values(int level, ILogTarget target, string name, string separator, object[] values)
+        {
+        }
+
+        public void Wrap(int level, ILogTarget target, string text)
+        {
+        }
+    }
+
+
     public class JobReporter : IJobReporter
     {
         private int m_ti;
