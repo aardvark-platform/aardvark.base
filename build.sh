055b6bffa96fae3b765e7d7991dae276b6aa4234
Harald Steinlechner <hs@vrvis.at>
2016-01-28 17:59:11 +0100

    again fixed travis build :D

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 mono .paket/paket.bootstrapper.exe
 mono .paket/paket.exe restore group Build
 
-mono packages/build/FAKE/tools/Fake.exe "build.fsx" Dummy --fsiargs build.fsx $@
+mono packages/Build/FAKE/tools/Fake.exe "build.fsx" Dummy --fsiargs build.fsx $@
