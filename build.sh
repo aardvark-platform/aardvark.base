57de2505ddc84adbbb3d36cffeddb1e55c7b50e6
Harald Steinlechner <hs@vrvis.at>
2016-01-28 18:25:18 +0100

    sync

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@
 mono .paket/paket.bootstrapper.exe
 mono .paket/paket.exe restore group Build
 
-mono packages/Build/FAKE/tools/Fake.exe "build.fsx" Dummy --fsiargs build.fsx $@
+
+mono packages/build/FAKE/tools/FAKE.exe "build.fsx" Dummy --fsiargs build.fsx $@
