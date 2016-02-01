123dc4399695a3579bcacbddb5c10bdbaf3404ca
Harald Steinlechner <hs@vrvis.at>
2016-02-01 11:56:06 +0100

    added InstancedMod test

--- src/Tests/Aardvark.Base.Incremental.Tests/Program.fs
+++ src/Tests/Aardvark.Base.Incremental.Tests/Program.fs
@@ -5,5 +5,7 @@ let main args =
     //Aardvark.Base.Incremental.Tests.SimpleTest.run()
     //Aardvark.Base.Incremental.Tests.``collect tests``.``[ASet] memory leak test``()
     //Aardvark.Base.Incremental.Tests.``collect tests``.``[ASet] async registerCallback``()
-    Aardvark.Base.Incremental.Tests.AgTests.``[Ag] Leaky leaky test``()
+    //Aardvark.Base.Incremental.Tests.AgTests.``[Ag] Leaky leaky test``()
+    
+    Aardvark.Base.Incremental.Tests.InstancingTest.test()
     0
