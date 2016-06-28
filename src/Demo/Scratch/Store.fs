40e977527888f8347a5a710ac38cb939604245a3
krauthaufen <krauthaufen@awx.at>
2016-06-28 09:36:44 +0200

    [Scratch] store sketch

--- src/Demo/Scratch/Store.fs
+++ src/Demo/Scratch/Store.fs
@@ -7,58 +7,13 @@ open System.Security.Cryptography
 open Aardvark.Base
 open Aardvark.Base.Native
 open System.IO
+open Microsoft.FSharp.NativeInterop
 
 #nowarn "9"
 
 let hash = MD5.Create()
 let md5 (str : string) =
     str |> Encoding.Unicode.GetBytes |> hash.ComputeHash |> Guid
-
-
-let sizeInvariantBench() =
-    let sw = Stopwatch()
-
-    let ids = Array.init (1 <<< 20) (fun i -> i |> string |> md5)
-    let data : byte[] = Array.init 128 byte
-    let data2 : byte[] = Array.zeroCreate 128
-
-    let path = @"C:\Users\Schorsch\Desktop\store.csv"
-    File.WriteAllLines(path, ["size;get [ns];write [ns];update [ns];delete[ns]"])
-
-    for cnt in 4096 .. 4096 .. 1 <<< 20 do
-        let ids = Array.take cnt ids
-        let mem = Memory.hglobal 0L
-        use store = new Store(mem)
-
-        printf "%d: " cnt
-        
-        sw.Restart()
-        let files = ids |> Array.map store.Get
-        sw.Stop()
-        let tget = sw.MicroTime / store.FileCount
-
-        sw.Restart()
-        for fi in 0..files.Length-1 do
-            files.[fi].Write data
-        sw.Stop()
-        let twrite = sw.MicroTime / files.Length
-        
-        sw.Restart()
-        for fi in 0..files.Length-1 do
-            files.[fi].Write data2
-        sw.Stop()
-        let tupdate = sw.MicroTime / files.Length
-
-        sw.Restart()
-        for fi in 0..files.Length-1 do
-            files.[fi].Delete()
-        sw.Stop()
-        let tdelete = sw.MicroTime / files.Length
-        
-        printfn "{ get = %A; write = %A; update = %A; delete = %A }" tget twrite tupdate tdelete
-        File.AppendAllLines(path, [sprintf "%d;%d;%d;%d;%d" cnt tget.TotalNanoseconds twrite.TotalNanoseconds tupdate.TotalNanoseconds tdelete.TotalNanoseconds])
-
-
 
 let perf() =
     let path = @"C:\Users\schorsch\Desktop\test.bin"
@@ -72,10 +27,11 @@ let perf() =
     sw.Restart()
     use store = new Store(mem)
     sw.Stop()
+
+    Log.line "memory:       %A" store.DataSize
+    Log.line "fileTable:    %A" store.FileTableSize
     if store.FileCount > 0 then
         Log.line "files:        %d" store.FileCount
-        Log.line "memory:       %A" store.DataSize
-        Log.line "fileTable:    %A" store.FileTableSize
         Log.line "per file:     %A" (sw.MicroTime / store.FileCount)
     Log.stop()
 
@@ -83,12 +39,11 @@ let perf() =
     sw.Restart()
     let files = ids |> Array.map store.Get
     sw.Stop()
-    Log.line "per file:     %A" (sw.MicroTime / store.FileCount)
+    Log.line "per file:     %A" (sw.MicroTime / files.Length)
     Log.stop()
 
 
-    let data : byte[] = Array.zeroCreate 128
-    let data2 : byte[] = Array.init 128 byte
+    let data : byte[] = Array.zeroCreate 44
 
     Log.startTimed "write"
     sw.Restart()
@@ -98,279 +53,221 @@ let perf() =
     Log.line "per file:     %A" (sw.MicroTime / files.Length)
     Log.stop()
 
-    Log.startTimed "update"
-    sw.Restart()
-    for fi in 0..files.Length-1 do
-        files.[fi].Write data2
-    sw.Stop()
-    Log.line "per file:     %A" (sw.MicroTime / files.Length)
-    Log.stop()
 
-    Log.startTimed "delete"
-    sw.Restart()
-    for fi in 0..files.Length-1 do
-        files.[fi].Delete()
-    sw.Stop()
-    Log.line "per file:     %A" (sw.MicroTime / files.Length)
-    Log.stop()
 
-    Log.startTimed "re-write"
-    sw.Restart()
-    for fi in 0..files.Length-1 do
-        files.[fi].Write data
-    sw.Stop()
-    Log.line "per file:     %A" (sw.MicroTime / files.Length)
-    Log.stop()
 
     ()
 
 
-module FileSystem =
-    open System.Runtime.InteropServices
 
-    [<Literal>]
-    let KeySize = 16
 
-    type Key = Guid
+module FS = 
 
-    [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
-    module Key =
-        let empty = Guid.Empty
+    let inline (++) (ptr : nativeptr<'a>) (v : 'a) = NativePtr.add ptr (int v)
+    let inline (!!) (ptr : nativeptr<'a>) = NativePtr.read ptr
+    let inline (<--) (ptr : nativeptr<'a>) c = NativePtr.write ptr c
 
-        let inline equals (a : Key) (b : Key) = a.Equals b
-        
-    [<StructLayout(LayoutKind.Sequential, Size = 96)>]
-    type header =
+    type Block =
         struct
-            val mutable public Magic : Guid
-            val mutable public Version : int
-            val mutable public FileCount : int
-            val mutable public FreeCount : int
-            val mutable public FreeList : int
-            val mutable public CapacityIndex : int
-            val mutable public Capacity : int
-            val mutable public BlockCount : int
-
-            static member inline Size = 96n
-        end
-
-    [<StructLayout(LayoutKind.Sequential)>]
-    type block =
-        struct
-            val mutable public Offset : int64
-            val mutable public Length : int64
-            val mutable public Next : int
-            val mutable public Prev : int
-
-            static member inline Size = 24n
+            val mutable public Offset   : int64
+            val mutable public Size     : int64
+            val mutable public IsFree   : int
+            val mutable public Prev     : int
+            val mutable public Next     : int
 
         end
 
-    [<StructLayout(LayoutKind.Sequential)>]
-    type entry =
+    type Entry =
         struct
-            val mutable public Value : block
+            val mutable public Block : Block
             val mutable public HashCode : int
             val mutable public Next : int
-            val mutable public Key : Key
+            val mutable public Key : Guid
+        end
 
-            static member inline Size = 32n + nativeint KeySize
+    type Node =
+        struct  
+            val mutable public Size : int
+            val mutable public Entry : int
+            val mutable public Left : int
+            val mutable public Right : int
+            val mutable public Parent : int
+
         end
 
 
-    type pheader(p : ptr) = 
-        inherit ptr<header>(p)
+    type blockptr =
+        struct
+            val mutable public ptr : nativeint
 
-        member x.Magic
-            with get()  = x.Read<Guid>()
-            and set v   = x.Write<Guid>(v)
+            static member inline (+) (ptr : blockptr, index : int) = blockptr(ptr.ptr + nativeint (sizeof<Block> * index))
 
-        member x.Version
-            with get()  = (x :> ptr + 16n).Read<int>()
-            and set v   = (x :> ptr + 16n).Write<int>(v)
+            member inline x.Offset      = NativePtr.ofNativeInt<int64>  x.ptr
+            member inline x.Size        = NativePtr.ofNativeInt<int64>  (8n + x.ptr)
+            member inline x.IsFree      = NativePtr.ofNativeInt<int>    (16n + x.ptr)
+            member inline x.Prev        = NativePtr.ofNativeInt<int>    (20n + x.ptr)
+            member inline x.Next        = NativePtr.ofNativeInt<int>    (24n + x.ptr)
+ 
+            new(p) = { ptr = p }
 
-        member x.FileCount
-            with get()  = (x :> ptr + 20n).Read<int>()
-            and set v   = (x :> ptr + 20n).Write<int>(v)
+        end
 
-        member x.FreeCount
-            with get()  = (x :> ptr + 24n).Read<int>()
-            and set v   = (x :> ptr + 24n).Write<int>(v)
+    type entryptr =
+        struct
+            val mutable public ptr : nativeint
 
-        member x.FreeList
-            with get()  = (x :> ptr + 28n).Read<int>()
-            and set v   = (x :> ptr + 28n).Write<int>(v)
+            member inline x.Block       = blockptr x.ptr
+            member inline x.HashCode    = NativePtr.ofNativeInt<int> (28n + x.ptr)
+            member inline x.Next        = NativePtr.ofNativeInt<int> (32n + x.ptr)
+            member inline x.Key         = NativePtr.ofNativeInt<Guid> (36n + x.ptr)
 
-        member x.CapacityIndex
-            with get()  = (x :> ptr + 32n).Read<int>()
-            and set v   = (x :> ptr + 32n).Write<int>(v)
+            static member inline (+) (ptr : entryptr, index : int) = entryptr(ptr.ptr + nativeint (sizeof<Entry> * index))
 
-        member x.Capacity
-            with get()  = (x :> ptr + 36n).Read<int>()
-            and set v   = (x :> ptr + 36n).Write<int>(v)
+            new(p) = { ptr = p }
+        end
 
-        member x.BlockCount
-            with get()  = (x :> ptr + 40n).Read<int>()
-            and set v   = (x :> ptr + 40n).Write<int>(v)
+    type nodeptr =
+        struct
+            val mutable public ptr : nativeint
 
-    type pblock(p : ptr) = 
-        inherit ptr<block>(p)
-
-        member private x.Pointer = p
-
-        member x.Offset
-            with get()  = x.Read<int64>()
-            and set v   = x.Write<int64>(v)
-
-        member x.Length
-            with get()  = (x :> ptr + 8n).Read<int64>()
-            and set v   = (x :> ptr + 8n).Write<int64>(v)
-
-        member x.Next
-            with get()  = (x :> ptr + 16n).Read<int>()
-            and set v   = (x :> ptr + 16n).Write<int>(v)
-
-        member x.Prev
-            with get()  = (x :> ptr + 20n).Read<int>()
-            and set v   = (x :> ptr + 20n).Write<int>(v)
-
-        static member (+) (pb : pblock, r : int) = pblock(pb.Pointer + nativeint r * block.Size)
-        static member (-) (pb : pblock, r : int) = pblock(pb.Pointer - nativeint r * block.Size)
-
-    type pentry(p : ptr) = 
-        inherit ptr<entry>(p)
-
-        member x.Pointer = p
+            member inline x.Size        = NativePtr.ofNativeInt<int> (x.ptr)
+            member inline x.Entry       = NativePtr.ofNativeInt<int> (4n + x.ptr)
+            member inline x.Left        = NativePtr.ofNativeInt<int> (8n + x.ptr)
+            member inline x.Right       = NativePtr.ofNativeInt<int> (12n + x.ptr)
+            member inline x.Parent      = NativePtr.ofNativeInt<int> (16n + x.ptr)
 
 
-        member x.ValuePtr = pblock(p)
+            static member inline (+) (ptr : nodeptr, index : int) = nodeptr(ptr.ptr + nativeint (sizeof<Node> * index))
 
-        member x.Value
-            with get()  = x.Read<block>()
-            and set v   = x.Write<block>(v)
+            new(p) = { ptr = p }
+        end
 
-        member x.HashCode
-            with get()  = (x :> ptr + 24n).Read<int>()
-            and set v   = (x :> ptr + 24n).Write<int>(v)
-
-        member x.Next
-            with get()  = (x :> ptr + 28n).Read<int>()
-            and set v   = (x :> ptr + 28n).Write<int>(v)
-
-        member x.Key
-            with get()  = (x :> ptr + 32n).Read<Key>()
-            and set v   = (x :> ptr + 32n).Write<Key>(v)
-
-        static member (+) (pb : pentry, r : int) = pentry(pb.Pointer + nativeint r * entry.Size)
-        static member (-) (pb : pentry, r : int) = pentry(pb.Pointer - nativeint r * entry.Size)
-        static member inline (+) (pb : pentry, r : 'a) = pentry(pb.Pointer + nativeint r * entry.Size)
-        static member inline (-) (pb : pentry, r : 'a) = pentry(pb.Pointer - nativeint r * entry.Size)
 
 
     type Store =
-        {
-            mem         : Memory
-            header      : pheader
-            entries     : pentry
-            buckets     : ptr<int>
-        }
+        class
+            
+            val mutable public First    : int
+            val mutable public Last     : int
+            val mutable public Root     : int
+            val mutable public Capacity : int
 
-    type Table() =
-        let store : Store = failwith "asdasd"
-        let header = store.header
+            val mutable public nodes    : nodeptr
+            val mutable public FreeList : nativeptr<int>
+            val mutable public Entries  : entryptr
+            val mutable public Buckets  : nativeptr<int>
 
-        member x.Grow() =
-            ()
+            member x.RemoveNode(ptr : nodeptr) =
+                failwith "implement me"
 
-        member x.FindEntry(key : Key, hashCode : int) =
-            let index = hashCode % header.Capacity
+            member x.AddNode(ptr : Node) : nodeptr =
+                failwith "implement me"
+                
 
-            let id = store.buckets.[index] - 1
-            if id < 0 then
-                -1
-            else
-                let e = store.entries + id
-                if e.HashCode = hashCode && Key.equals e.Key key then
-                    id
-                else
-                    let next = e.Next
-                    if next < 0 then
-                        -1
+            member x.GrowTable() =
+                failwith "implement me"
+
+            member x.GrowData(needed : int64) =
+                failwith "implement me"
+
+
+            member x.GetFree(size : int64) =
+                let mutable best = nodeptr(0n)
+                let mutable current = x.Root
+
+                // search for the smallest node >= size
+                while current >= 0 && current < x.Capacity do
+                    let pNode = x.nodes + current
+
+                    let c = compare (int size) !!pNode.Size
+
+                    if c > 0 then
+                        current <- !!pNode.Right
+                    elif c < 0 then
+                        best <- pNode
+                        current <- !!pNode.Left
                     else
-                        let rec search (id : int) (e : pentry) =
-                            if e.HashCode = hashCode && Key.equals e.Key key then
+                        best <- pNode
+                        current <- -1
+
+                // if we found a best-fit use it (removing it from the tree)
+                if best.ptr <> 0n then
+                    let e = !!best.Entry
+                    let pEntry = x.Entries + e
+
+                    let n = !!pEntry.Next
+                    if n >= 0 then
+                        best.Entry <-- n
+                    else
+                        x.RemoveNode best
+
+                    pEntry
+                
+                // otherwise increase memory-size and retry
+                else
+                    x.GrowData size
+                    x.GetFree size
+
+            member x.Alloc(size : int64) =
+                let pEntry = x.GetFree(size)
+                let pBlock = pEntry.Block
+                if !!pBlock.Size > size then
+                    failwith "split me"
+
+                pBlock.Size <-- size
+                pBlock.IsFree <-- 0
+
+                pEntry
+
+            member x.NewFile(key : Guid, size : int64) =
+                let hashCode = key.GetHashCode() &&& 0x7FFFFFFF
+                let pEntry = x.Alloc size
+                pEntry.Key <-- key
+                pEntry.HashCode <-- hashCode
+
+                let index = (pEntry.ptr - x.Entries.ptr) / nativeint sizeof<Entry> |> int
+                let pBucket = x.Buckets ++ hashCode % x.Capacity
+                let ei = !!pBucket - 1
+
+                if ei < 0 then
+                    pBucket <-- index + 1
+                else
+                    pEntry.Next <-- ei
+                    pBucket <-- index + 1
+
+            member x.FindEntry(key : Guid, hashCode : int) =
+                let pBucket = x.Buckets ++ hashCode % x.Capacity
+                let ei = !!pBucket - 1
+
+                if ei < 0 then 
+                    -1
+                else
+                    let pEntry = x.Entries + ei
+                    if !!pEntry.HashCode = hashCode && !!pEntry.Key = key then
+                        ei
+                    else
+                        let rec search (id : int) (pEntry : entryptr) =
+                            if id < 0 then
+                                -1
+                            elif !!pEntry.HashCode = hashCode && !!pEntry.Key = key then
                                 id
                             else
-                                let next = e.Next
-                                if next >= 0 then
-                                    search next (store.entries + next)
-                                else
-                                    -1
+                                let n = !!pEntry.Next
+                                search n (x.Entries + n)
 
-                        search next (store.entries + next)
+                        let n = !!pEntry.Next
+                        search n (x.Entries + n)
 
-
-        member x.Alloc() =
-            let freeCount = header.FreeCount
-            if freeCount > 0 then
-                let id = header.FreeList
-                let entry = store.entries + id
-                header.FreeList <- entry.Next
-                header.FreeCount <- freeCount - 1
-                entry.Next <- 0
-                id
-            else
-                let count = header.BlockCount
-                if count = header.Capacity then
-                    x.Grow()
-
-                header.BlockCount <- count + 1
-                count
-
-        member x.Free(eid : int) =
-            let next = header.FreeList
-            let e = store.entries + eid
-
-            e.Value <- block()
-            e.HashCode <- -1
-            e.Next <- next
-            e.Key <- Key.empty
+            member x.Delete(key : Guid) =
+                let hashCode = key.GetHashCode() &&& 0x7FFFFFFF
+                let id = x.FindEntry(key, hashCode)
+                if id >= 0 then
+                    
 
 
-            header.FreeList <- eid
-            header.FreeCount <- header.FreeCount + 1
-            
-        member x.Get(eid : int) =
-            store.entries + eid
+                    true
+                else
+                    false
 
-
-
-    //[<CustomEquality; NoComparison>]
-    type Block(t : Table, id : int) =
-        
-        member x.Id = id
-
-        override x.GetHashCode() =
-            id
-
-        override x.Equals o =
-            match o with
-                | :? Block as o -> x.Id = o.Id
-                | _ -> false
-
-        member x.Offset
-            with get() = t.Get(id).ValuePtr.Offset
-            and set (n : int64) = t.Get(id).ValuePtr.Offset <- n
-
-        member x.Size
-            with get() = t.Get(id).ValuePtr.Length
-            and set (n : int64) = t.Get(id).ValuePtr.Length <- n
-
-        member x.Next
-            with get() = Block(t, t.Get(id).ValuePtr.Next)
-            and set (n : Block) = t.Get(id).ValuePtr.Next <- n.Id
-
-        member x.Prev
-            with get() = Block(t, t.Get(id).ValuePtr.Prev)
-            and set (n : Block) = t.Get(id).ValuePtr.Prev <- n.Id
+        end
 
