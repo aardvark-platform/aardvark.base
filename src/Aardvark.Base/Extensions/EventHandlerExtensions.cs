fa748dc47686e1ae609f039eba6d9e85a64fe9b5
Christian Luksch <luksch@vrvis.at>
2016-12-22 17:03:08 +0100

    added EventHandlerExtensions;

---
+++ src/Aardvark.Base/Extensions/EventHandlerExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Aardvark.Base
+{
+    public static class EventHandlerExtensions
+    {
+        /// <summary>
+        /// Safely invokes the event using null as sender and EventArgs.Empty
+        /// </summary>
+        public static void TryInvoke(this EventHandler eh)
+        {
+            eh?.Invoke(null, EventArgs.Empty);
+        }
+
+        /// <summary>
+        /// Safely invokes the event using the supplied sender with EventArgs.Empty
+        /// </summary>
+        public static void TryInvoke(this EventHandler eh, object sender)
+        {
+            eh?.Invoke(sender, EventArgs.Empty);
+        }
+    }
+}
