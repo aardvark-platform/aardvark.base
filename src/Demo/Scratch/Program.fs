ed5633e58bd11ab18bd708f3491687af9aca8d9e
krauthaufen <krauthaufen@awx.at>
2017-04-10 10:04:10 +0200

    [Base] simple graph tools

--- src/Demo/Scratch/Program.fs
+++ src/Demo/Scratch/Program.fs
@@ -16,7 +16,17 @@ open System.Windows.Forms
 
 [<EntryPoint; STAThread>]
 let main argv = 
-    React.Test.run()
+    let rand = RandomSystem()
+    let g = UndirectedGraph.ofNodes (Set.ofList [0..127]) (fun li ri -> float (ri - li) |> Some)
+
+    let tree = UndirectedGraph.maximumSpanningTree compare g
+    printfn "%A" tree
+
+    printfn "%A" (Tree.weight tree / float (Tree.count tree))
+
+
+
+    //React.Test.run()
     Environment.Exit 0
 
     0 // return an integer exit code
