be283a9af16ac25f0e4f04a1f5e2de69c96f1171
Stefan Maierhofer <sm@stefanmaierhofer.com>
2017-04-15 09:46:45 +0200

    removed annoying cls in build.cmd ;-)

--- build.cmd
+++ build.cmd
@@ -13,8 +13,6 @@ if errorlevel 1 (
   exit /b %errorlevel%
 )
 
-cls
-
 SET FSI_PATH=packages\build\FAKE\tools\Fake.exe
 "%FSI_PATH%" "build.fsx" Dummy --fsiargs build.fsx --shadowcopyreferences+ %* 
 
