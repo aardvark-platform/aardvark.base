437a60ec13c3897f26ec9b5013b6354e1db861fc
krauthaufen <krauthaufen@awx.at>
2017-05-18 09:08:02 +0200

    [Geometry] added PolygonTessellator - exposing low-level access to LibTessDotNet - defining Combine/Triangulate extensions on seq<V(2|3)d[]> and seq<Polygon(2|3)d>

--- src/Aardvark.Geometry/PolyRegion2d.fs
+++ src/Aardvark.Geometry/PolyRegion2d.fs
@@ -1,6 +1,7 @@
 ﻿namespace Aardvark.Geometry
 
 open System
+open System.Runtime.CompilerServices
 open Aardvark.Base
 open LibTessDotNet.Double
 
@@ -20,8 +21,23 @@ module private LibTess =
 
         arr
 
-    let pruneRedundantPoints (angleEps : float) (p : Polygon2d) : Option<Polygon2d> =
-        if p.PointCount < 3 then
+    let closed3 (path : array<V3d>) =
+        let isClosed = path.[0] = path.[path.Length-1]
+           
+        let arr = Array.zeroCreate (path.Length + (if isClosed then 0 else 1))
+
+        for i in 0 .. path.Length - 1 do
+            let pt = path.[i]
+            arr.[i] <- ContourVertex(Position = Vec3(X = pt.X, Y = pt.Y, Z = pt.Z))
+                        
+        if not isClosed then
+            arr.[path.Length] <- arr.[0]
+
+
+        arr
+
+    let nonRedundantPoints (angleEps : float) (p : V2d[]) : Option<int[]> =
+        if p.Length < 3 then
             None
         else
             let inline angle (a : V2d) (b : V2d) =
@@ -31,29 +47,29 @@ module private LibTess =
                 else Constant.Pi - asin c
                     
             
-            let mutable pl = p.[p.PointCount - 1]
+            let mutable pl = p.[p.Length - 1]
             let mutable pc = p.[0]
             let mutable pn = p.[1]
 
             let mutable dlc = pc - pl |> Vec.normalize
             let mutable dcn = pn - pc |> Vec.normalize
 
-            let points = System.Collections.Generic.List<V2d>()
-            for i in 0 .. p.PointCount - 1 do
+            let points = System.Collections.Generic.List<int>()
+            for i in 0 .. p.Length - 1 do
                 let a = angle dlc dcn
                 if abs a > angleEps then
-                    points.Add(pc)
+                    points.Add(i)
 
                 pl <- pc
                 pc <- pn
-                pn <- p.[(i + 2) % p.PointCount]
+                pn <- p.[(i + 2) % p.Length]
                 dlc <- dcn
                 dcn <- pn - pc |> Vec.normalize 
 
             if points.Count < 3 then 
                 None
-            elif points.Count = p.PointCount then Some p
-            else Some (Polygon2d (points.ToArray()))
+            else 
+                Some (points.ToArray())
 
     let boundary (rule : WindingRule) (regions : seq<list<Polygon2d>>) : list<Polygon2d> =
         let t = Tess()
@@ -76,9 +92,9 @@ module private LibTess =
             for pi in 0 .. 2 .. indices.Length - 1 do
                 let start = indices.[pi + 0]
                 let count = indices.[pi + 1]
-                let poly = Polygon2d (Array.init count (fun vi -> vertices.[start + vi]))
-                match pruneRedundantPoints Constant.PositiveTinyValue poly with
-                    | Some p -> results.Add p
+                let poly = Array.init count (fun vi -> vertices.[start + vi])
+                match nonRedundantPoints Constant.PositiveTinyValue poly with
+                    | Some index -> results.Add (Polygon2d (index |> Array.map (Array.get poly)))
                     | _ -> ()
 
             results |> CSharpList.toList
@@ -105,14 +121,376 @@ module private LibTess =
                 Triangle2d(vertices.[i0], vertices.[i1], vertices.[i2])
             )
 
+type TessellationRule =
+    | EvenOdd = 0
+    | NonZero = 1
+    | Positive = 2
+    | Negative = 3
+    | AbsGreater1 = 4
 
+type Triangle2d<'a> =
+    struct
+        val mutable public P0 : V2d
+        val mutable public P1 : V2d
+        val mutable public P2 : V2d
+
+        val mutable public A0 : 'a
+        val mutable public A1 : 'a
+        val mutable public A2 : 'a
+
+        new(p0, a0, p1, a1, p2, a2) = { P0 = p0; P1 = p1; P2 = p2; A0 = a0; A1 = a1; A2 = a2 }
+    end
+
+type Polygon2d<'a> =
+    struct
+        val mutable public Points : V2d[]
+        val mutable public Attributes : 'a[]
+
+        new(p,a) = { Points = p; Attributes = a }
+    end
+
+type Triangle3d<'a> =
+    struct
+        val mutable public P0 : V3d
+        val mutable public P1 : V3d
+        val mutable public P2 : V3d
+
+        val mutable public A0 : 'a
+        val mutable public A1 : 'a
+        val mutable public A2 : 'a
+
+        new(p0, a0, p1, a1, p2, a2) = { P0 = p0; P1 = p1; P2 = p2; A0 = a0; A1 = a1; A2 = a2 }
+    end
+
+type Polygon3d<'a> =
+    struct
+        val mutable public Points : V3d[]
+        val mutable public Attributes : 'a[]
+
+        new(p,a) = { Points = p; Attributes = a }
+    end
+
+[<AbstractClass; Sealed; Extension>]
+type PolygonTessellator private() =
+
+    [<Extension>]
+    static member Combine (regions : seq<V2d[] * 'a[]>, rule : TessellationRule, interpolate : float[] -> 'a[] -> 'a) =
+        let t = Tess()
+                
+        for (pts, att) in regions do
+            let contour = LibTess.closed pts
+            for i in 0 .. contour.Length - 1 do contour.[i].Data <- att.[i]
+            t.AddContour(contour)
+
+        let combine (pos : Vec3) (data : obj[]) (weights : float[]) =
+            interpolate weights (data |> Array.map unbox) :> obj
+
+        t.Tessellate(unbox (int rule), ElementType.BoundaryContours, 2, CombineCallback(combine))
+        
+        if isNull t.Elements || isNull t.Vertices then
+            []
+        else
+            let results = System.Collections.Generic.List<V2d[] * 'a[]>()
+
+            let indices = t.Elements
+            let vertices = t.Vertices |> Array.map (fun v -> V2d(v.Position.X, v.Position.Y), unbox<'a> v.Data)
+
+            for pi in 0 .. 2 .. indices.Length - 2 do
+                let start = indices.[pi + 0]
+                let count = indices.[pi + 1]
+
+                let points = Array.zeroCreate count
+                let att = Array.zeroCreate count
+                for vi in 0 .. count - 1 do
+                    let (p,a) = vertices.[start + vi]
+                    points.[vi] <- p
+                    att.[vi] <- a
+
+                results.Add(points, att)
+
+            CSharpList.toList results
+            
+    [<Extension>]
+    static member Combine (regions : seq<V2d[]>, rule : TessellationRule) =
+        let t = Tess()
+                
+        for pts in regions do
+            let contour = LibTess.closed pts
+            t.AddContour(contour)
+
+        t.Tessellate(unbox (int rule), ElementType.BoundaryContours, 2)
+        
+        if isNull t.Elements || isNull t.Vertices then
+            []
+        else
+            let results = System.Collections.Generic.List<V2d[]>()
+
+            let indices = t.Elements
+            let vertices = t.Vertices |> Array.map (fun v -> V2d(v.Position.X, v.Position.Y))
+
+            for pi in 0 .. 2 .. indices.Length - 2 do
+                let start = indices.[pi + 0]
+                let count = indices.[pi + 1]
+
+                let points = Array.init count (fun vi -> vertices.[start + vi])
+                results.Add(points)
+
+            CSharpList.toList results
+            
+    [<Extension>]
+    static member Combine (regions : seq<Polygon2d<'a>>, rule : TessellationRule, interpolate : float[] -> 'a[] -> 'a) =
+        let regions = regions |> Seq.map (fun p -> p.Points, p.Attributes)
+        PolygonTessellator.Combine(regions, rule, interpolate)
+        
+    [<Extension>]
+    static member Combine (regions : seq<Polygon2d>, rule : TessellationRule) =
+        let regions = regions |> Seq.map (fun p -> Seq.toArray p.Points)
+        PolygonTessellator.Combine(regions, rule)
+        
+    [<Extension>]
+    static member Triangulate (regions : seq<V2d[] * 'a[]>, rule : TessellationRule, interpolate : float[] -> 'a[] -> 'a) : list<Triangle2d<'a>> =
+        let t = Tess()
+                
+        for (pts, att) in regions do
+            let contour = LibTess.closed pts
+            for i in 0 .. contour.Length - 1 do contour.[i].Data <- att.[i]
+            t.AddContour(contour)
+
+        let combine (pos : Vec3) (data : obj[]) (weights : float[]) =
+            interpolate weights (data |> Array.map unbox) :> obj
+
+        t.Tessellate(unbox (int rule), ElementType.Polygons, 3, CombineCallback(combine))
+        
+        if isNull t.Elements || isNull t.Vertices then
+            []
+        else
+            let results = System.Collections.Generic.List<Triangle2d<'a>>()
+
+            let indices = t.Elements
+            let vertices = t.Vertices |> Array.map (fun v -> V2d(v.Position.X, v.Position.Y), unbox<'a> v.Data)
+
+            for pi in 0 .. 3 .. indices.Length - 3 do
+                let i0 = indices.[pi + 0]
+                let i1 = indices.[pi + 1]
+                let i2 = indices.[pi + 2]
+
+                let (p0, a0) = vertices.[i0]
+                let (p1, a1) = vertices.[i1]
+                let (p2, a2) = vertices.[i2]
+
+                results.Add(Triangle2d<'a>(p0, a0, p1, a1, p2, a2))
+
+
+            CSharpList.toList results
+            
+    [<Extension>]
+    static member Triangulate (regions : seq<V2d[]>, rule : TessellationRule) =
+        let t = Tess()
+                
+        for pts in regions do
+            let contour = LibTess.closed pts
+            t.AddContour(contour)
+
+        t.Tessellate(unbox (int rule), ElementType.Polygons, 3)
+        
+        if isNull t.Elements || isNull t.Vertices then
+            []
+        else
+            let results = System.Collections.Generic.List<Triangle2d>()
+
+            let indices = t.Elements
+            let vertices = t.Vertices |> Array.map (fun v -> V2d(v.Position.X, v.Position.Y))
+
+            for pi in 0 .. 3 .. indices.Length - 3 do
+                let i0 = indices.[pi + 0]
+                let i1 = indices.[pi + 1]
+                let i2 = indices.[pi + 2]
+
+                let p0 = vertices.[i0]
+                let p1 = vertices.[i1]
+                let p2 = vertices.[i2]
+
+                results.Add(Triangle2d(p0, p1, p2))
+
+
+            CSharpList.toList results
+
+    [<Extension>]
+    static member Triangulate (regions : seq<Polygon2d<'a>>, rule : TessellationRule, interpolate : float[] -> 'a[] -> 'a) =
+        let regions = regions |> Seq.map (fun p -> p.Points, p.Attributes)
+        PolygonTessellator.Triangulate(regions, rule, interpolate)
+        
+    [<Extension>]
+    static member Triangulate (regions : seq<Polygon2d>, rule : TessellationRule) =
+        let regions = regions |> Seq.map (fun p -> Seq.toArray p.Points)
+        PolygonTessellator.Triangulate(regions, rule)
+
+
+
+
+    [<Extension>]
+    static member Combine (regions : seq<V3d[] * 'a[]>, rule : TessellationRule, interpolate : float[] -> 'a[] -> 'a) =
+        let t = Tess()
+                
+        for (pts, att) in regions do
+            let contour = LibTess.closed3 pts
+            for i in 0 .. contour.Length - 1 do contour.[i].Data <- att.[i]
+            t.AddContour(contour)
+
+        let combine (pos : Vec3) (data : obj[]) (weights : float[]) =
+            interpolate weights (data |> Array.map unbox) :> obj
+
+        t.Tessellate(unbox (int rule), ElementType.BoundaryContours, 2, CombineCallback(combine))
+        
+        if isNull t.Elements || isNull t.Vertices then
+            []
+        else
+            let results = System.Collections.Generic.List<V3d[] * 'a[]>()
+
+            let indices = t.Elements
+            let vertices = t.Vertices |> Array.map (fun v -> V3d(v.Position.X, v.Position.Y, v.Position.Z), unbox<'a> v.Data)
+
+            for pi in 0 .. 2 .. indices.Length - 2 do
+                let start = indices.[pi + 0]
+                let count = indices.[pi + 1]
+
+                let points = Array.zeroCreate count
+                let att = Array.zeroCreate count
+                for vi in 0 .. count - 1 do
+                    let (p,a) = vertices.[start + vi]
+                    points.[vi] <- p
+                    att.[vi] <- a
+
+                results.Add(points, att)
+
+            CSharpList.toList results
+         
+    [<Extension>]
+    static member Combine (regions : seq<V3d[]>, rule : TessellationRule) =
+        let t = Tess()
+                
+        for pts in regions do
+            let contour = LibTess.closed3 pts
+            t.AddContour(contour)
+
+        t.Tessellate(unbox (int rule), ElementType.BoundaryContours, 2)
+        
+        if isNull t.Elements || isNull t.Vertices then
+            []
+        else
+            let results = System.Collections.Generic.List<V3d[]>()
+
+            let indices = t.Elements
+            let vertices = t.Vertices |> Array.map (fun v -> V3d(v.Position.X, v.Position.Y, v.Position.Z))
+
+            for pi in 0 .. 2 .. indices.Length - 2 do
+                let start = indices.[pi + 0]
+                let count = indices.[pi + 1]
+
+                let points = Array.init count (fun vi -> vertices.[start + vi])
+                results.Add(points)
+
+            CSharpList.toList results
+
+    [<Extension>]
+    static member Combine (regions : seq<Polygon3d<'a>>, rule : TessellationRule, interpolate : float[] -> 'a[] -> 'a) =
+        let regions = regions |> Seq.map (fun p -> p.Points, p.Attributes)
+        PolygonTessellator.Combine(regions, rule, interpolate)
+        
+    [<Extension>]
+    static member Combine (regions : seq<Polygon3d>, rule : TessellationRule) =
+        let regions = regions |> Seq.map (fun p -> Seq.toArray p.Points)
+        PolygonTessellator.Combine(regions, rule)
+  
+    [<Extension>]
+    static member Triangulate (regions : seq<V3d[] * 'a[]>, rule : TessellationRule, interpolate : float[] -> 'a[] -> 'a) : list<Triangle3d<'a>> =
+        let t = Tess()
+                
+        for (pts, att) in regions do
+            let contour = LibTess.closed3 pts
+            for i in 0 .. contour.Length - 1 do contour.[i].Data <- att.[i]
+            t.AddContour(contour)
+
+        let combine (pos : Vec3) (data : obj[]) (weights : float[]) =
+            interpolate weights (data |> Array.map unbox) :> obj
+
+        t.Tessellate(unbox (int rule), ElementType.Polygons, 3, CombineCallback(combine))
+        
+        if isNull t.Elements || isNull t.Vertices then
+            []
+        else
+            let results = System.Collections.Generic.List<Triangle3d<'a>>()
+
+            let indices = t.Elements
+            let vertices = t.Vertices |> Array.map (fun v -> V3d(v.Position.X, v.Position.Y, v.Position.Z), unbox<'a> v.Data)
+
+            for pi in 0 .. 3 .. indices.Length - 3 do
+                let i0 = indices.[pi + 0]
+                let i1 = indices.[pi + 1]
+                let i2 = indices.[pi + 2]
+
+                let (p0, a0) = vertices.[i0]
+                let (p1, a1) = vertices.[i1]
+                let (p2, a2) = vertices.[i2]
+
+                results.Add(Triangle3d<'a>(p0, a0, p1, a1, p2, a2))
+
+
+            CSharpList.toList results
+    
+    [<Extension>]
+    static member Triangulate (regions : seq<V3d[]>, rule : TessellationRule) =
+        let t = Tess()
+                
+        for pts in regions do
+            let contour = LibTess.closed3 pts
+            t.AddContour(contour)
+
+        t.Tessellate(unbox (int rule), ElementType.Polygons, 3)
+        
+        if isNull t.Elements || isNull t.Vertices then
+            []
+        else
+            let results = System.Collections.Generic.List<Triangle3d>()
+
+            let indices = t.Elements
+            let vertices = t.Vertices |> Array.map (fun v -> V3d(v.Position.X, v.Position.Y, v.Position.Z))
+
+            for pi in 0 .. 3 .. indices.Length - 3 do
+                let i0 = indices.[pi + 0]
+                let i1 = indices.[pi + 1]
+                let i2 = indices.[pi + 2]
+
+                let p0 = vertices.[i0]
+                let p1 = vertices.[i1]
+                let p2 = vertices.[i2]
+
+                results.Add(Triangle3d(p0, p1, p2))
+
+
+            CSharpList.toList results
+  
+    [<Extension>]
+    static member Triangulate (regions : seq<Polygon3d<'a>>, rule : TessellationRule, interpolate : float[] -> 'a[] -> 'a) =
+        let regions = regions |> Seq.map (fun p -> p.Points, p.Attributes)
+        PolygonTessellator.Triangulate(regions, rule, interpolate)
+        
+    [<Extension>]
+    static member Triangulate (regions : seq<Polygon3d>, rule : TessellationRule) =
+        let regions = regions |> Seq.map (fun p -> Seq.toArray p.Points)
+        PolygonTessellator.Triangulate(regions, rule)
+           
+
+/// PolyRegion represents a set of non-overlapping, counterclockwise polygons surrounding
+/// the intended region.
+/// When constructed using a polygon the implementation enforces these invariants
 type PolyRegion private(polygons : list<Polygon2d>) =
     static let empty = PolyRegion []
     
     static member Empty = empty
     static member Zero = empty
 
-    member x.polygons = polygons
+    member x.Polygons = polygons
 
     member x.Triangulate() =
         LibTess.triangulate WindingRule.EvenOdd polygons
@@ -125,21 +503,27 @@ type PolyRegion private(polygons : list<Polygon2d>) =
     member x.Transformed(m : M33d) =
         PolyRegion (polygons |> List.map (fun p -> p.Transformed m))
 
+    member x.BoundingBox =
+        Box2d(polygons |> Seq.collect (fun p -> p.Points))
+
+    interface IBoundingBox2d with
+        member x.BoundingBox2d = x.BoundingBox
+
     member x.IsEmpty =
         List.isEmpty polygons
 
     static member Union(l : PolyRegion, r : PolyRegion) =
-        LibTess.boundary WindingRule.Positive [l.polygons; r.polygons] |> PolyRegion
+        LibTess.boundary WindingRule.Positive [l.Polygons; r.Polygons] |> PolyRegion
 
     static member Difference(l : PolyRegion, r : PolyRegion) =
-        let rev = r.polygons |> List.map (fun p -> p.Reversed)
-        LibTess.boundary WindingRule.Positive [l.polygons; rev] |> PolyRegion
+        let rev = r.Polygons |> List.map (fun p -> p.Reversed)
+        LibTess.boundary WindingRule.Positive [l.Polygons; rev] |> PolyRegion
 
     static member Intersection(l : PolyRegion, r : PolyRegion) =
-        LibTess.boundary WindingRule.AbsGeqTwo [l.polygons; r.polygons] |> PolyRegion
+        LibTess.boundary WindingRule.AbsGeqTwo [l.Polygons; r.Polygons] |> PolyRegion
 
     static member Xor(l : PolyRegion, r : PolyRegion) =
-        LibTess.boundary WindingRule.EvenOdd [l.polygons; r.polygons] |> PolyRegion
+        LibTess.boundary WindingRule.EvenOdd [l.Polygons; r.Polygons] |> PolyRegion
 
     static member inline (^^^) (l : PolyRegion, r : PolyRegion) = PolyRegion.Xor(l,r)
     static member inline (*) (l : PolyRegion, r : PolyRegion) = PolyRegion.Intersection(l,r)
@@ -152,10 +536,14 @@ type PolyRegion private(polygons : list<Polygon2d>) =
     member x.Overlaps(other : PolyRegion) =
         (other * x).IsEmpty |> not
 
-    new(p : Polygon2d) =
-        let res = LibTess.boundary WindingRule.EvenOdd [[p]]
+
+    new(p : Polygon2d, tess : TessellationRule) =
+        let res = LibTess.boundary (unbox (int tess)) [[p]]
         let res = res |> List.map (fun p -> if p.IsCcw() then p else p.Reversed)
         PolyRegion(res)
+
+    new(p : Polygon2d) =
+        PolyRegion(p, TessellationRule.EvenOdd)
             
 
 [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
@@ -169,8 +557,9 @@ module PolyRegion =
     let inline ofBox (b : Box2d) = b.ToPolygon2dCCW() |> ofPolygon
     
     let inline transformed (m : M33d) (r : PolyRegion) = r.Transformed m
+    let inline bounds (r : PolyRegion) = r.BoundingBox
 
-    let inline toPolygons (r : PolyRegion) = r.polygons
+    let inline toPolygons (r : PolyRegion) = r.Polygons
     let inline toTriangles (r : PolyRegion) = r.Triangulate()
 
     let inline union (l : PolyRegion) (r : PolyRegion) = l + r
--- src/Aardvark.Geometry/Program.fs
+++ src/Aardvark.Geometry/Program.fs
@@ -1,79 +1,84 @@
 ﻿namespace Aardvark.Geometry
 
-//open Aardvark.Geometry
-//open Aardvark.Base
-//open System.Drawing
-//open System.Drawing.Imaging
-//
-//
-//let transform (p : V2d) =
-//    Point(int ((p.X * 400.0) + 25.0), int ((p.Y * 400.0) + 25.0))
-//
-//let closed (path : array<V2d>) =
-//    let isClosed = path.[0] = path.[path.Length-1]
-//           
-//    let arr = Array.zeroCreate (path.Length + (if isClosed then 0 else 1))
-//
-//    for i in 0 .. path.Length - 1 do
-//        let pt = path.[i]
-//        arr.[i] <- transform pt
-//                        
-//    if not isClosed then
-//        arr.[path.Length] <- arr.[0]
-//
-//
-//    arr
-//
-//type Graphics with
-//    member x.DrawPolygon(pen : Pen, p : Polygon2d) =
-//        let pts = p.Points |> Seq.toArray |> closed
-//        x.DrawPolygon(pen, pts)
-//        
-//    member x.DrawPolygon(pen : Pen, p : PolyRegion) =
-//        for p in p.polygons do
-//            x.DrawPolygon(pen, p)
-//
-//    member x.FillPolygon(pen : Brush, p : Polygon2d) =
-//        let pts = p.Points |> Seq.toArray |> closed
-//        x.FillPolygon(pen, pts)
-//        
-//    member x.FillPolygon(pen : Brush, p : PolyRegion) =
-//        for p in p.polygons do
-//            x.FillPolygon(pen, p)
-//
-//[<EntryPoint>]
-//let main args =
-//
-//    let p0 = Polygon2d [| V2d(0.0,0.0); V2d(1.0,0.0); V2d(1.0,1.0); V2d(0.0,1.0) |] |> PolyRegion.ofPolygon
-//    let p1 = p0 |> PolyRegion.transformed (M33d.Translation(0.5, 0.5))
-//    //let res = PolyRegion.xor p0 p1
-//    //let res = PolyRegion.difference p0 p0
-//    let res = PolyRegion.union p0 p1
-//
-//
-//    let triangles = PolyRegion.toTriangles res
-//
-//    use bmp = new Bitmap(1024, 768, PixelFormat.Format24bppRgb)
-//    use g = Graphics.FromImage bmp
-//
-//    g.Clear(Color.Black)
-//    g.DrawPolygon(Pens.Yellow, p0)
-//    g.DrawPolygon(Pens.Yellow, p1)
-//
-//    g.FillPolygon(Brushes.Red, res)
-//
-//    for t in triangles do
-//        g.DrawPolygon(Pens.White, t.ToPolygon2d(id))
-//
-//    g.DrawPolygon(new Pen(Color.Blue, 3.0f), res)
-//    let s = 16
-//    for p in res.polygons do
-//        for p in p.Points do
-//            let pp = transform p
-//            g.FillEllipse(Brushes.Green, Rectangle(pp.X - s / 2, pp.Y - s / 2, s, s))
-//
-//
-//
-//    bmp.Save @"C:\Users\Schorsch\Desktop\poly.jpg"
-//
-//    0
+module internal Demo =
+    open Aardvark.Geometry
+    open Aardvark.Base
+    open System.Drawing
+    open System.Drawing.Imaging
+
+    let transform (s : Size) (bounds : Box2d) (p : V2d) =
+        let r = (p - bounds.Min) / bounds.Size
+        Point(int (r.X * float s.Width), int (r.Y * float s.Height))
+
+    let closed (s : Size) (bounds : Box2d) (path : array<V2d>) =
+        let isClosed = path.[0] = path.[path.Length-1]
+           
+        let arr = Array.zeroCreate (path.Length + (if isClosed then 0 else 1))
+
+        for i in 0 .. path.Length - 1 do
+            let pt = path.[i]
+            arr.[i] <- transform s bounds pt
+                        
+        if not isClosed then
+            arr.[path.Length] <- arr.[0]
+
+
+        arr
+
+    type Graphics with
+        member x.DrawPolygon(pen : Pen, p : Polygon2d, bounds : Box2d) =
+            let size = Size(int x.VisibleClipBounds.Width, int x.VisibleClipBounds.Height)
+            let pts = p.Points |> Seq.toArray |> closed size bounds
+            x.DrawPolygon(pen, pts)
+        
+        member x.DrawPolygon(pen : Pen, p : PolyRegion, bounds : Box2d) =
+            for p in p.Polygons do
+                x.DrawPolygon(pen, p, bounds)
+
+        member x.FillPolygon(pen : Brush, p : Polygon2d, bounds : Box2d) =
+            let size = Size(int x.VisibleClipBounds.Width, int x.VisibleClipBounds.Height)
+            let pts = p.Points |> Seq.toArray |> closed size bounds
+            x.FillPolygon(pen, pts)
+        
+        member x.FillPolygon(pen : Brush, p : PolyRegion, bounds : Box2d) =
+            for p in p.Polygons do
+                x.FillPolygon(pen, p, bounds)
+
+    //[<EntryPoint>]
+    let main args =
+
+        let p0 = PolyRegion.ofArray [| V2d(0.0,0.0); V2d(1.0,0.0); V2d(1.0,1.0); V2d(0.0,1.0) |]
+        let p1 = p0 |> PolyRegion.transformed (M33d.Translation(0.5, 0.5))
+        
+        let res = PolyRegion.union p0 p1
+
+
+        let bounds = Box2d [ PolyRegion.bounds p0; PolyRegion.bounds p1 ]
+        let bounds = bounds.EnlargedByRelativeEps(0.2)
+
+        let triangles = PolyRegion.toTriangles res
+
+        use bmp = new Bitmap(1024, 768, PixelFormat.Format24bppRgb)
+        use g = Graphics.FromImage bmp
+
+        g.Clear(Color.Black)
+        g.DrawPolygon(Pens.Yellow, p0, bounds)
+        g.DrawPolygon(Pens.Yellow, p1, bounds)
+
+        g.FillPolygon(Brushes.Red, res, bounds)
+
+        for t in triangles do
+            g.DrawPolygon(Pens.White, t.ToPolygon2d(id), bounds)
+
+        g.DrawPolygon(new Pen(Color.Blue, 3.0f), res, bounds)
+        let s = 16
+        for p in res.Polygons do
+            for p in p.Points do
+                let pp = transform bmp.Size bounds p
+                g.FillEllipse(Brushes.Green, Rectangle(pp.X - s / 2, pp.Y - s / 2, s, s))
+
+
+
+        bmp.Save @"C:\Users\Schorsch\Desktop\poly.jpg"
+
+        0
