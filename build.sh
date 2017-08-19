e2e8f014d714c9700923ae893742d732523505ce
Stefan Maierhofer <sm@stefanmaierhofer.com>
2017-08-19 21:09:02 +0200

    fixed warning in build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mono .paket/paket.bootstrapper.exe
-mono .paket/paket.exe restore group Build
+mono .paket/paket.exe restore --group Build
 
 
 #mono packages/build/FAKE/tools/FAKE.exe "build.fsx" Dummy --fsiargs build.fsx $@
