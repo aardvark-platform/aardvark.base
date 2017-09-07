a15edee318389a0ce294c9454fd989a071047cc5
Stefan Maierhofer <sm@stefanmaierhofer.com>
2017-09-07 14:57:55 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,4 @@
 mono .paket/paket.bootstrapper.exe
 mono .paket/paket.exe restore --group Build
 
-
-#mono packages/build/FAKE/tools/FAKE.exe "build.fsx" Dummy --fsiargs build.fsx $@
-echo "currently our build script does not work on mono >= 5.0. for that reason we use vanilla builds (which makes addsource etc unavailable in that form. you need to use fsi sessions for portions of the build script which fork. please do so and create issues describing the mono/fsharp issues)"
-mono .paket/paket.exe install
-msbuild src/Aardvark.sln
+mono packages/build/FAKE/tools/FAKE.exe "build.fsx" Dummy --fsiargs build.fsx $@
