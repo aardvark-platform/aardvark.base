dc5ccbbe717ad811243c86c0e95bb561742145d9
Stefan Maierhofer <sm@stefanmaierhofer.com>
2017-10-20 14:24:58 +0200

    add Aardvark.Base.Cell and tests

---
+++ src/Aardvark.Base/RangesBoxes/Cell.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Aardvark.Base
+{
+    /// <summary>
+    /// A 2^Exponent sized cube positioned at (X,Y,Z) * 2^Exponent.
+    /// </summary>
+    public struct Cell : IEquatable<Cell>
+    {
+        /// <summary>
+        /// Unit cell (0, 0, 0, 1) -> Box3d[(0.0, 0.0, 0.0), (1.0, 1.0, 1.0)]
+        /// </summary>
+        public static readonly Cell Unit = new Cell(0, 0, 0, 1);
+
+        /// <summary>
+        /// </summary>
+        public readonly long X;
+
+        /// <summary>
+        /// </summary>
+        public readonly long Y;
+
+        /// <summary>
+        /// </summary>
+        public readonly long Z;
+
+        /// <summary>
+        /// </summary>
+        public readonly int Exponent;
+
+        /// <summary>
+        /// Creates a 2^exponent sized cube positioned at (x,y,z) * 2^exponent.
+        /// </summary>
+        public Cell(long x, long y, long z, int exponent)
+        {
+            X = x; Y = y; Z = z; Exponent = exponent;
+            BoundingBox = ComputeBoundingBox(X, Y, Z, Exponent);
+        }
+
+        /// <summary>
+        /// Cell with min corner at index*2^exponent and dimension 2^exponent.
+        /// </summary>
+        public Cell(V3l index, int exponent) : this(index.X, index.Y, index.Z, exponent) { }
+
+        /// <summary>
+        /// Special cell, which is centered at origin, with dimension 2^exponent.
+        /// </summary>
+        public Cell(int exponent) : this(long.MaxValue, long.MaxValue, long.MaxValue, exponent) { }
+
+        /// <summary>
+        /// Smallest cell that contains given box.
+        /// </summary>
+        public Cell(Box3f box) : this(new Box3d(box)) { }
+
+        /// <summary>
+        /// Smallest cell that contains given points.
+        /// </summary>
+        public Cell(V3f[] ps) : this(new Box3f(ps)) { }
+
+        /// <summary>
+        /// Smallest cell that contains given points.
+        /// </summary>
+        public Cell(V3d[] ps) : this(new Box3d(ps)) { }
+
+        /// <summary>
+        /// Smallest cell that contains given points.
+        /// </summary>
+        public Cell(IEnumerable<V3f> ps) : this(new Box3f(ps)) { }
+
+        /// <summary>
+        /// Smallest cell that contains given points.
+        /// </summary>
+        public Cell(IEnumerable<V3d> ps) : this(new Box3d(ps)) { }
+
+        /// <summary>
+        /// Smallest cell that contains given box.
+        /// </summary>
+        public Cell(V3d p) : this(new Box3d(p)) { }
+
+        /// <summary>
+        /// Smallest cell that contains given box.
+        /// </summary>
+        public Cell(Box3d box)
+        {
+            if (!box.IsValid) throw new InvalidOperationException();
+
+            // case 1: contains origin
+            if (box.Min.X < 0.0 && box.Max.X > 0.0 ||
+                box.Min.Y < 0.0 && box.Max.Y > 0.0 ||
+                box.Min.Z < 0.0 && box.Max.Z > 0.0)
+            {
+                X = Y = Z = long.MaxValue;
+                Exponent = Math.Max(box.Min.NormMax, box.Max.NormMax).Log2Int() + 1;
+            }
+            else // case 2: doesn't contain origin
+            {
+                Exponent = (box.Min == box.Max)
+                        ? (box.Min.NormMax / (long.MaxValue >> 1)).Log2Int()
+                        : box.Size.NormMax.Log2Int()
+                        ;
+                var s = Math.Pow(2.0, Exponent);
+                var a = (box.Min / s).Floor * s;
+                while (a.X + s < box.Max.X || a.Y + s < box.Max.Y || a.Z + s < box.Max.Z)
+                {
+                    s *= 2.0; Exponent++;
+                    a = (box.Min / s).Floor * s;
+                }
+                X = (long)Math.Floor(a.X / s);
+                Y = (long)Math.Floor(a.Y / s);
+                Z = (long)Math.Floor(a.Z / s);
+            }
+
+            BoundingBox = ComputeBoundingBox(X, Y, Z, Exponent);
+        }
+
+        /// <summary>
+        /// Gets whether this cell is a special cell centered at origin.
+        /// </summary>
+        public bool IsCenteredAtOrigin => X == long.MaxValue && Y == long.MaxValue && Z == long.MaxValue;
+
+        /// <summary>
+        /// Returns true if the cell completely contains the other cell.
+        /// </summary>
+        public bool Contains(Cell other)
+        {
+            if (other.Exponent > Exponent) return false;
+            if (other.Exponent == Exponent) return other.X == X && other.Y == Y && other.Z == Z;
+
+            if (IsCenteredAtOrigin)
+            {
+                if (other.IsCenteredAtOrigin)
+                {
+                    return Exponent >= other.Exponent;
+                }
+                else
+                {
+                    if (Exponent > other.Exponent)
+                    {
+                        // bring other into scale of this
+                        var d = Exponent - other.Exponent;
+
+                        // right-shift with long argument only uses low-order 6 bits
+                        // https://docs.microsoft.com/en-us/dotnet/articles/csharp/language-reference/operators/right-shift-operator
+                        if (d > 63) d = 63;
+
+                        var x = other.X >> d;
+                        var y = other.Y >> d;
+                        var z = other.Z >> d;
+                        return x >= -1 && x <= 0 && y >= -1 && y <= 0 && z >= -1 && z <= 0;
+                    }
+                    else
+                    {
+                        throw new InvalidOperationException();
+                    }
+                }
+            }
+            else
+            {
+                if (other.IsCenteredAtOrigin)
+                {
+                    return false;
+                }
+                else
+                {
+                    if (Exponent > other.Exponent)
+                    {
+                        // bring other into scale of this
+                        var d = Exponent - other.Exponent;
+
+                        // right-shift with long argument only uses low-order 6 bits
+                        // https://docs.microsoft.com/en-us/dotnet/articles/csharp/language-reference/operators/right-shift-operator
+                        if (d > 63) d = 63;
+
+                        var x = other.X >> d;
+                        var y = other.Y >> d;
+                        var z = other.Z >> d;
+                        return x == X && y == Y && z == Z;
+                    }
+                    else
+                    {
+                        // bring this into scale of other
+                        var d = other.Exponent - Exponent;
+
+                        // right-shift with long argument only uses low-order 6 bits
+                        // https://docs.microsoft.com/en-us/dotnet/articles/csharp/language-reference/operators/right-shift-operator
+                        if (d > 63) d = 63;
+
+                        var x = X >> d;
+                        var y = Y >> d;
+                        var z = Z >> d;
+                        return x == other.X && y == other.Y && z == other.Z;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns true if two cells intersect each other.
+        /// </summary>
+        public bool Intersects(Cell other)
+        {
+            return BoundingBox.Intersects(other.BoundingBox);
+        }
+
+        /// <summary>
+        /// Gets indices of the 8 subcells.
+        /// </summary>
+        public Cell[] Children
+        {
+            get
+            {
+                var x0 = X << 1; var y0 = Y << 1; var z0 = Z << 1;
+                var x1 = x0 + 1; var y1 = y0 + 1; var z1 = z0 + 1;
+                var e = Exponent - 1;
+                return new[]
+                {
+                    new Cell(x0, y0, z0, e),
+                    new Cell(x1, y0, z0, e),
+                    new Cell(x0, y1, z0, e),
+                    new Cell(x1, y1, z0, e),
+                    new Cell(x0, y0, z1, e),
+                    new Cell(x1, y0, z1, e),
+                    new Cell(x0, y1, z1, e),
+                    new Cell(x1, y1, z1, e),
+                };
+            }
+        }
+
+        /// <summary>
+        /// Gets cell's bounds.
+        /// </summary>
+        public Box3d BoundingBox { get; }
+
+        /// <summary>
+        /// Octant 0-7.
+        /// 0th, 1st and 2nd bit encodes x-, y-, z-axis, respectively.
+        /// E.g. 0 is octant at origin, 7 is octant oposite from origin.
+        /// </summary>
+        public Cell GetOctant(int i)
+        {
+            if (i < 0 || i > 7) throw new IndexOutOfRangeException();
+
+            if (IsCenteredAtOrigin)
+            {
+                var e = Exponent - 1;
+                return new Cell(
+                    (i & 1) == 0 ? -1 : 0,
+                    (i & 2) == 0 ? -1 : 0,
+                    (i & 4) == 0 ? -1 : 0,
+                    Exponent - 1
+                    );
+            }
+
+            return new Cell(
+                (X << 1) + ((i & 1) == 0 ? 0 : 1),
+                (Y << 1) + ((i & 2) == 0 ? 0 : 1),
+                (Z << 1) + ((i & 4) == 0 ? 0 : 1),
+                Exponent - 1
+                );
+        }
+
+        #region equality and hashing
+
+        /// <summary>
+        /// </summary>
+        public bool Equals(Cell other) => this == other;
+
+        /// <summary>
+        /// </summary>
+        public static bool operator ==(Cell a, Cell b) => a.X == b.X && a.Y == b.Y && a.Z == b.Z && a.Exponent == b.Exponent;
+
+        /// <summary>
+        /// </summary>
+        public static bool operator !=(Cell a, Cell b) => !(a == b);
+
+        /// <summary>
+        /// </summary>
+        public override bool Equals(object obj) => obj is Cell && this == (Cell)obj;
+
+        /// <summary>
+        /// </summary>
+        public override int GetHashCode() => HashCode.GetCombined(X, Y, Z, Exponent);
+
+        #endregion
+        
+        #region binary serialization
+
+        /// <summary>
+        /// </summary>
+        public byte[] ToByteArray()
+        {
+            var buffer = new byte[28];
+            using (var ms = new MemoryStream(buffer))
+            using (var bw = new BinaryWriter(ms))
+            {
+                bw.Write(X); bw.Write(Y); bw.Write(Z); bw.Write(Exponent);
+            }
+            return buffer;
+        }
+
+        /// <summary>
+        /// </summary>
+        public static Cell Parse(byte[] buffer, int offset = 0)
+        {
+            using (var ms = new MemoryStream(buffer, 0, 28))
+            using (var br = new BinaryReader(ms))
+            {
+                var x = br.ReadInt64(); var y = br.ReadInt64(); var z = br.ReadInt64(); var e = br.ReadInt32();
+                return new Cell(x, y, z, e);
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// </summary>
+        public override string ToString()
+        {
+            return $"[{X}, {Y}, {Z}, {Exponent}]";
+        }
+
+        private static Box3d ComputeBoundingBox(long x, long y, long z, int e)
+        {
+            var d = Math.Pow(2.0, e);
+            var isCenteredAtOrigin = x == long.MaxValue && y == long.MaxValue && z == long.MaxValue;
+            var min = isCenteredAtOrigin ? new V3d(-0.5 * d) : new V3d(x * d, y * d, z * d);
+            return Box3d.FromMinAndSize(min, new V3d(d, d, d));
+        }
+    }
+}
---
+++ src/Tests/Aardvark.Base.Tests/RangesBoxes/CellTests.cs
@@ -0,0 +1,448 @@
+﻿/*
+    Copyright (C) 2017. Aardvark Platform Team. http://github.com/aardvark-platform.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+using Aardvark.Base;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace Aardvark.Tests
+{
+    [TestFixture]
+    public class CellTests
+    {
+        #region creation
+
+        [Test]
+        public void CanCreateCell()
+        {
+            var a = new Cell(1, 2, 3, -1);
+            Assert.IsTrue(a.X == 1 && a.Y == 2 && a.Z == 3 && a.Exponent == -1);
+        }
+
+        [Test]
+        public void CanCreateCell2()
+        {
+            var a = new Cell(1, 2, 3, -1);
+            var b = new Cell(new V3l(1, 2, 3), -1);
+            Assert.IsTrue(a == b);
+        }
+
+        [Test]
+        public void CanCreateCell_CenteredAtOrigin()
+        {
+            var a = new Cell(1).BoundingBox;
+            Assert.IsTrue(a.Min == new V3d(-1, -1, -1));
+            Assert.IsTrue(a.Max == new V3d(+1, +1, +1));
+        }
+
+        [Test]
+        public void CanCreateCell_FromBox()
+        {
+            var a = new Cell(new Box3d(new V3d(-1.3, 0, 0), new V3d(0.1, 1, 1)));
+            Assert.IsTrue(a == new Cell(2));
+        }
+
+        [Test]
+        public void CanCreateCell_FromBoxf()
+        {
+            var a = new Cell(new Box3f(new V3f(-1.3, 0, 0), new V3f(0.1, 1, 1)));
+            Assert.IsTrue(a == new Cell(2));
+        }
+
+        [Test]
+        public void CanCreateCell_FromV3ds()
+        {
+            var a = new Cell(new[] { new V3d(-1.3, 0, 0), new V3d(0.1, 1, 1) });
+            Assert.IsTrue(a == new Cell(2));
+        }
+
+        [Test]
+        public void CanCreateCell_FromV3fs()
+        {
+            var a = new Cell(new[] { new V3f(-1.3, 0, 0), new V3f(0.1, 1, 1) });
+            Assert.IsTrue(a == new Cell(2));
+        }
+
+        [Test]
+        public void CanCreateCell_FromV3dEnumerable()
+        {
+            var a = new Cell((IEnumerable<V3d>)new[] { new V3d(-1.3, 0, 0), new V3d(0.1, 1, 1) });
+            Assert.IsTrue(a == new Cell(2));
+        }
+
+        [Test]
+        public void CanCreateCell_FromV3fEnumerable()
+        {
+            var a = new Cell((IEnumerable<V3f>)new[] { new V3f(-1.3, 0, 0), new V3f(0.1, 1, 1) });
+            Assert.IsTrue(a == new Cell(2));
+        }
+
+        [Test]
+        public void CanCreateCell_FromBox2()
+        {
+            var a = new Cell(new Box3d(new V3d(0.1, 0.8, 0.8), new V3d(0.8, 0.9, 0.9)));
+            Assert.IsTrue(a == new Cell(0, 0, 0, 0));
+        }
+
+        [Test]
+        public void CanCreateCell_FromBox3()
+        {
+            var a = new Cell(new Box3d(new V3d(1.1, 0.8, 0.8), new V3d(1.8, 0.9, 0.9)));
+            Assert.IsTrue(a == new Cell(1, 0, 0, 0));
+        }
+
+        [Test]
+        public void CanCreateCell_FromPoint()
+        {
+            var p = new V3d(0.1, 0.2, -0.3);
+            var a = new Cell(p);
+            Assert.IsTrue(a.BoundingBox.Contains(p));
+        }
+
+        [Test]
+        public void CanCreateCell_FromPointAtPowerOfTwoLocation()
+        {
+            for (var e = -1024; e <= 1023; e++)
+            {
+                var p = new V3d(Math.Pow(2.0, e));
+                var a = new Cell(p);
+                Assert.IsTrue(a.BoundingBox.Contains(p));
+            }
+        }
+
+        [Test]
+        public void CanCreateCell_FromPoint_Origin()
+        {
+            var p = V3d.Zero;
+            var a = new Cell(p);
+            Assert.IsTrue(a.BoundingBox.Contains(p));
+        }
+
+        [Test]
+        public void CanCreateCell_FromPoint_MaxNegative()
+        {
+            var p = new V3d(-double.MaxValue, -double.MaxValue, -double.MaxValue);
+            var a = new Cell(p);
+            Assert.IsTrue(a.BoundingBox.Contains(p));
+        }
+
+        [Test]
+        public void CanCreateCell_FromPoint_MaxPositive()
+        {
+            var p = new V3d(double.MaxValue, double.MaxValue, double.MaxValue);
+            var a = new Cell(p);
+            Assert.IsTrue(a.BoundingBox.Contains(p));
+        }
+
+        #endregion
+
+        #region serialization
+        
+        [Test]
+        public void CanSerializeCellToBinary()
+        {
+            var a = new Cell(1, 2, 3, -1);
+            var buffer = a.ToByteArray();
+        }
+
+        [Test]
+        public void CanDeserializeCellFromBinary()
+        {
+            var a = new Cell(1, 2, 3, -1);
+            var buffer = a.ToByteArray();
+
+            var b = Cell.Parse(buffer);
+
+            Assert.IsTrue(a == b);
+        }
+
+        #endregion
+
+        #region CellIsCenteredAtOrigin
+
+        [Test]
+        public void CellIsCenteredAtOrigin()
+        {
+            var a = new Cell(10);
+            Assert.IsTrue(a.IsCenteredAtOrigin);
+        }
+
+        [Test]
+        public void CellIsNotCenteredAtOrigin()
+        {
+            var a = new Cell(1, 2, 3, 4);
+            Assert.IsTrue(!a.IsCenteredAtOrigin);
+        }
+
+        #endregion
+
+        #region equality
+
+        [Test]
+        public void CellIsEqualToItself()
+        {
+            var a = new Cell(1, 2, 3, -1);
+
+            Assert.IsTrue(a.Equals((object)a));
+            Assert.IsTrue(a.Equals(a));
+        }
+
+        [Test]
+        public void Cell_Equality()
+        {
+            var a = new Cell(1, 2, 3, -1);
+            var b = new Cell(1, 2, 3, -1);
+
+            Assert.IsTrue(a.Equals((object)b));
+            Assert.IsTrue(a.Equals(b));
+            Assert.IsTrue(a == b);
+        }
+
+        [Test]
+        public void Cell_Inequality()
+        {
+            Assert.IsTrue(new Cell(0, 0, 0, 0) != new Cell(1, 0, 0, 0));
+            Assert.IsTrue(new Cell(0, 0, 0, 0) != new Cell(0, 1, 0, 0));
+            Assert.IsTrue(new Cell(0, 0, 0, 0) != new Cell(0, 0, 1, 0));
+            Assert.IsTrue(new Cell(0, 0, 0, 0) != new Cell(0, 0, 0, 1));
+        }
+
+        [Test]
+        public void Cell_GetHashCode()
+        {
+            var a = new Cell(1, 0, 0, 0).GetHashCode();
+            var b = new Cell(0, 1, 0, 0).GetHashCode();
+            var c = new Cell(0, 0, 1, 0).GetHashCode();
+            var d = new Cell(0, 0, 0, 1).GetHashCode();
+
+            Assert.IsTrue(a != b && a != c && a != d && b != c && b != d && c != d);
+        }
+
+        #endregion
+
+        #region ToString
+
+        [Test]
+        public void Cell_ToString()
+        {
+            var s = new Cell(1, 2, 3, -1).ToString();
+            Assert.IsTrue(s == "[1, 2, 3, -1]");
+        }
+
+        #endregion
+
+        #region BoundingBox
+
+        [Test]
+        public void Cell_Box3d()
+        {
+            Assert.IsTrue(new Cell(0, 0, 0, 0).BoundingBox == new Box3d(new V3d(0, 0, 0), new V3d(1, 1, 1)));
+            Assert.IsTrue(new Cell(0, 0, 0, 1).BoundingBox == new Box3d(new V3d(0, 0, 0), new V3d(2, 2, 2)));
+            Assert.IsTrue(new Cell(1, 2, 3, 4).BoundingBox == new Box3d(new V3d(16, 32, 48), new V3d(32, 48, 64)));
+            Assert.IsTrue(new Cell(0, 0, 0, -1).BoundingBox == new Box3d(new V3d(0, 0, 0), new V3d(0.5, 0.5, 0.5)));
+            Assert.IsTrue(new Cell(-1, 0, 0, -1).BoundingBox == new Box3d(new V3d(-0.5, 0, 0), new V3d(0.0, 0.5, 0.5)));
+        }
+
+        #endregion
+
+        #region children
+
+        [Test]
+        public void Cell_Children1()
+        {
+            var xs = new Cell(0, 0, 0, 0).Children;
+            Assert.IsTrue(xs[0] == new Cell(0, 0, 0, -1));
+            Assert.IsTrue(xs[1] == new Cell(1, 0, 0, -1));
+            Assert.IsTrue(xs[2] == new Cell(0, 1, 0, -1));
+            Assert.IsTrue(xs[3] == new Cell(1, 1, 0, -1));
+            Assert.IsTrue(xs[4] == new Cell(0, 0, 1, -1));
+            Assert.IsTrue(xs[5] == new Cell(1, 0, 1, -1));
+            Assert.IsTrue(xs[6] == new Cell(0, 1, 1, -1));
+            Assert.IsTrue(xs[7] == new Cell(1, 1, 1, -1));
+        }
+
+        [Test]
+        public void Cell_Children2()
+        {
+            var xs = new Cell(-2, 0, 10, 1).Children;
+            Assert.IsTrue(xs[0] == new Cell(-4, 0, 20, 0));
+            Assert.IsTrue(xs[1] == new Cell(-3, 0, 20, 0));
+            Assert.IsTrue(xs[2] == new Cell(-4, 1, 20, 0));
+            Assert.IsTrue(xs[3] == new Cell(-3, 1, 20, 0));
+            Assert.IsTrue(xs[4] == new Cell(-4, 0, 21, 0));
+            Assert.IsTrue(xs[5] == new Cell(-3, 0, 21, 0));
+            Assert.IsTrue(xs[6] == new Cell(-4, 1, 21, 0));
+            Assert.IsTrue(xs[7] == new Cell(-3, 1, 21, 0));
+        }
+
+        [Test]
+        public void Cell_GetOctant()
+        {
+            var c = new Cell(-2, 0, 10, 1);
+            var xs = new[] { c.GetOctant(0), c.GetOctant(1), c.GetOctant(2), c.GetOctant(3), c.GetOctant(4), c.GetOctant(5), c.GetOctant(6), c.GetOctant(7) };
+            Assert.IsTrue(xs[0] == new Cell(-4, 0, 20, 0));
+            Assert.IsTrue(xs[1] == new Cell(-3, 0, 20, 0));
+            Assert.IsTrue(xs[2] == new Cell(-4, 1, 20, 0));
+            Assert.IsTrue(xs[3] == new Cell(-3, 1, 20, 0));
+            Assert.IsTrue(xs[4] == new Cell(-4, 0, 21, 0));
+            Assert.IsTrue(xs[5] == new Cell(-3, 0, 21, 0));
+            Assert.IsTrue(xs[6] == new Cell(-4, 1, 21, 0));
+            Assert.IsTrue(xs[7] == new Cell(-3, 1, 21, 0));
+        }
+
+        #endregion
+
+        #region inside/outside tests
+
+        [Test]
+        public void Cell_InsideOutside_ContainsEqual()
+        {
+            var a = new Cell(1, 2, 3, 4);
+            var b = new Cell(1, 2, 3, 4);
+            Assert.IsTrue(a.Contains(b));
+            Assert.IsTrue(b.Contains(a));
+        }
+
+        [Test]
+        public void Cell_InsideOutside_Contains_Inside()
+        {
+            var a = new Cell(0, 0, 0, 0);
+            var b = new Cell(1, 2, 3, -4);
+            Assert.IsTrue(a.Contains(b));
+        }
+
+        [Test]
+        public void Cell_InsideOutside_Contains_InsideTouching()
+        {
+            var a = new Cell(0, 0, 0, 0);
+            var b = new Cell(0, 0, 0, -1);
+            Assert.IsTrue(a.Contains(b));
+        }
+
+        [Test]
+        public void Cell_InsideOutside_Contains_Outside()
+        {
+            var a = new Cell(0, 0, 0, 0);
+            var b = new Cell(2, 0, 0, 0);
+            Assert.IsTrue(!a.Contains(b));
+        }
+
+        [Test]
+        public void Cell_InsideOutside_Contains_OutsideTouching()
+        {
+            var a = new Cell(0, 0, 0, 0);
+            var b = new Cell(1, 0, 0, 0);
+            Assert.IsTrue(!a.Contains(b));
+        }
+
+        [Test]
+        public void Cell_InsideOutside_Contains_Centered1()
+        {
+            var a = new Cell(1);
+            Assert.IsTrue(a.Contains(new Cell(-1, -1, -1, 0)));
+            Assert.IsTrue(a.Contains(new Cell(0, -1, -1, 0)));
+            Assert.IsTrue(a.Contains(new Cell(-1, 0, -1, 0)));
+            Assert.IsTrue(a.Contains(new Cell(0, 0, -1, 0)));
+            Assert.IsTrue(a.Contains(new Cell(-1, -1, 0, 0)));
+            Assert.IsTrue(a.Contains(new Cell(0, -1, 0, 0)));
+            Assert.IsTrue(a.Contains(new Cell(-1, 0, 0, 0)));
+            Assert.IsTrue(a.Contains(new Cell(0, 0, 0, 0)));
+        }
+
+        [Test]
+        public void Cell_InsideOutside_Contains_Centered2()
+        {
+            var a = new Cell(1);
+            var b = new Cell(0);
+            Assert.IsTrue(a.Contains(b));
+        }
+
+        [Test]
+        public void Cell_InsideOutside_Contains_Centered3()
+        {
+            var a = new Cell(1);
+            var b = new Cell(0);
+            Assert.IsTrue(!b.Contains(a));
+        }
+
+        [Test]
+        public void Cell_InsideOutside_Contains_Centered4()
+        {
+            var a = new Cell(2);
+            var b = new Cell(2);
+            Assert.IsTrue(a.Contains(b));
+            Assert.IsTrue(b.Contains(a));
+        }
+
+        [Test]
+        public void Cell_InsideOutside_Contains_Centered5()
+        {
+            var a = new Cell(2);
+            var b = new Cell(0, 0, 0, 2);
+            Assert.IsTrue(!a.Contains(b));
+        }
+
+        [Test]
+        public void Cell_InsideOutside_Contains_Centered6()
+        {
+            var a = new Cell(2);
+            var b = new Cell(10, 10, 10, 1);
+            Assert.IsTrue(!a.Contains(b));
+        }
+
+        [Test]
+        public void Cell_InsideOutside_Contains_Centered7()
+        {
+            var a = new Cell(2);
+            var b = new Cell(0, 0, 0, 3);
+            Assert.IsTrue(!a.Contains(b));
+        }
+
+        [Test]
+        public void Cell_InsideOutside_Contains_Centered8()
+        {
+            var a = new Cell(0, 0, 0, 2);
+            var b = new Cell(1);
+            Assert.IsTrue(!a.Contains(b));
+        }
+
+        [Test]
+        public void Cell_InsideOutside_Intersects_OutsideTouchingMin()
+        {
+            var a = new Cell(2, 0, 0, 0);
+            var b = new Cell(1, 0, 0, 0);
+            Assert.IsTrue(!a.Intersects(b));
+        }
+
+        [Test]
+        public void Cell_InsideOutside_Intersects_OutsideTouchingMax()
+        {
+            var a = new Cell(1, 0, 0, 0);
+            var b = new Cell(2, 0, 0, 0);
+            Assert.IsTrue(!a.Intersects(b));
+        }
+
+        #endregion
+
+        [Test]
+        public void Cell_InsideOutside_Contains_ExponentDeltaGreaterThan63()
+        {
+            // {[0, 0, 0, 3]}
+            // {[4408771833616582656, 3234636573324969984, 4552195268789674496, -62]}
+            var a = new Cell(0, 0, 0, 3);
+            var b = new Cell(4408771833616582656, 3234636573324969984, 4552195268789674496, -62);
+            Assert.IsTrue(a.Contains(b));
+        }
+    }
+}
