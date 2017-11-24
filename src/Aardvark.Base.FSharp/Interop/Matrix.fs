3c642a108cfd66207a6eea1d5c9e2bd1cbb37460
krauthaufen <krauthaufen@awx.at>
2017-11-24 08:53:13 +0100

    [Base] matrix conversion functions

--- src/Aardvark.Base.FSharp/Interop/Matrix.fs
+++ src/Aardvark.Base.FSharp/Interop/Matrix.fs
@@ -25,3 +25,33 @@ module Trafo =
     
     let inline forward (t : Trafo3d) = t.Forward
     let inline backward (t : Trafo3d) = t.Backward
+
+
+#nowarn "77"
+
+[<AutoOpen>]
+module MatrixConverters =
+
+    let inline private conv< ^a, ^b when (^a or ^b) : (static member op_Explicit : ^a -> ^b) > (a : ^a) : ^b =
+        ((^a or ^b) : (static member op_Explicit : ^a -> ^b) (a))
+    
+
+    let inline m22d (v : ^a) = conv< ^a, M22d > v
+    let inline m33d (v : ^a) = conv< ^a, M33d > v
+    let inline m34d (v : ^a) = conv< ^a, M34d > v
+    let inline m44d (v : ^a) = conv< ^a, M44d > v
+    
+    let inline m22f (v : ^a) = conv< ^a, M22f > v
+    let inline m33f (v : ^a) = conv< ^a, M33f > v
+    let inline m34f (v : ^a) = conv< ^a, M34f > v
+    let inline m44f (v : ^a) = conv< ^a, M44f > v
+
+    let inline m22i (v : ^a) = conv< ^a, M22i > v
+    let inline m33i (v : ^a) = conv< ^a, M33i > v
+    let inline m34i (v : ^a) = conv< ^a, M34i > v
+    let inline m44i (v : ^a) = conv< ^a, M44i > v
+
+    let inline m22l (v : ^a) = conv< ^a, M22l > v
+    let inline m33l (v : ^a) = conv< ^a, M33l > v
+    let inline m34l (v : ^a) = conv< ^a, M34l > v
+    let inline m44l (v : ^a) = conv< ^a, M44l > v
