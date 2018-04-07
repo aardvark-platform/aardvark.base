c158356de68a29904378c3e6dd9299b09f8ab3df
Stefan Maierhofer <sm@stefanmaierhofer.com>
2018-04-07 11:09:48 +0200

    moved more extensions from Aardvark.Base to netstandard

---
+++ src/Aardvark.Base.Sorting/ListExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Aardvark.Base.Sorting;
+using System;
+using System.Collections.Generic;
+
+namespace Aardvark.Base
+{
+    public static class ListOrderingExtensions
+    {
+        public static int NSmallestIndex<T>(this List<T> a, int n)
+            where T : IComparable<T>
+        {
+            if (n == 0) return a.SmallestIndex();
+            var p = a.CreatePermutationQuickMedianAscending(n);
+            return p[n];
+        }
+
+        public static int NLargestIndex<T>(this List<T> a, int n)
+            where T : IComparable<T>
+        {
+            if (n == 0) return a.LargestIndex();
+            var p = a.CreatePermutationQuickMedianDescending(n);
+            return p[n];
+        }
+    }
+}
