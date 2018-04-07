c25bfb5f98dadb81053432a088bdf5396f3165d7
Stefan Maierhofer <sm@stefanmaierhofer.com>
2018-04-07 08:17:03 +0200

    moved Aardvark.Base/Algodat/XmlParser.cs to Aardvark.Base.IO/XmlParser.cs

--- src/Aardvark.Base/AlgoDat/Structs.cs
+++ src/Aardvark.Base/AlgoDat/Structs.cs
@@ -1,41 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.Contracts;
-using System.Linq;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Diagnostics.Contracts;
+//using System.Linq;
+//using System.Text;
 
-namespace Aardvark.Base
-{
-    #region Volatile Array
+//namespace Aardvark.Base
+//{
+//    #region Volatile Array
 
-    /// <summary>
-    /// A wrapper for a normal array for safe parallel (multi-threaded) writing.
-    /// </summary>
-    public struct VolatileArray<T>
-    {
-        public volatile T[] Array;
+//    /// <summary>
+//    /// A wrapper for a normal array for safe parallel (multi-threaded) writing.
+//    /// </summary>
+//    public struct VolatileArray<T>
+//    {
+//        public volatile T[] Array;
 
-        public VolatileArray(T[] array) { Array = array; }
-    }
+//        public VolatileArray(T[] array) { Array = array; }
+//    }
 
-    #endregion
+//    #endregion
 
-    #region StructsExtensions
+//    #region StructsExtensions
 
-    public static class StructsExtensions
-    {
-        #region IndexedValue
+//    public static class StructsExtensions
+//    {
+//        #region IndexedValue
 
-        public static IndexedValue<T> IndexedValue<T>(this T self, int index) => new IndexedValue<T>(index, self);
+//        public static IndexedValue<T> IndexedValue<T>(this T self, int index) => new IndexedValue<T>(index, self);
 
-        #endregion
+//        #endregion
         
-        #region VolatileArray
+//        #region VolatileArray
 
-        public static VolatileArray<T> ToVolatile<T>(this T[] array) => new VolatileArray<T>(array);
+//        public static VolatileArray<T> ToVolatile<T>(this T[] array) => new VolatileArray<T>(array);
 
-        #endregion
-    }
+//        #endregion
+//    }
 
-    #endregion
-}
+//    #endregion
+//}
