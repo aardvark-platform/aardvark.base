482dcecee50a8ac9fad86e0a799bbd7180369a13
krauthaufen <krauthaufen@awx.at>
2018-04-10 12:31:18 +0200

    Rendering.Text using Unofficial.Typography
    Former-commit-id: a54c8bb6d3b35e0b99f475d50de7b2475ee566a5

--- src/Aardvark.Rendering.Text/Font.fs
+++ src/Aardvark.Rendering.Text/Font.fs
@@ -12,7 +12,6 @@ open System.Runtime.CompilerServices
 open Aardvark.Base
 open Aardvark.Base.Rendering
 open System.Drawing
-open System.Drawing.Drawing2D
 
 
 [<Flags>]
@@ -21,101 +20,6 @@ type FontStyle =
     | Bold = 1
     | Italic = 2
     | BoldItalic = 3
-
-module private GDI32 =
-
-    
-    open System.Runtime.InteropServices
-    open Microsoft.FSharp.NativeInterop
-    open System.Collections.Generic
-
-
-    [<AutoOpen>]
-    module private Wrappers = 
-        [<StructLayout(LayoutKind.Sequential)>]
-        type KerningPair =
-            struct
-                val mutable public first : uint16
-                val mutable public second : uint16
-                val mutable public amount : int
-            end
-
-        [<StructLayout(LayoutKind.Sequential)>]
-        type ABC =
-            struct
-                val mutable public A : float32
-                val mutable public B : float32
-                val mutable public C : float32
-            end
-
-        [<DllImport("gdi32.dll")>]
-        extern int GetKerningPairsW (nativeint hdc, int count, KerningPair* pairs)
-
-        [<DllImport("gdi32.dll")>]
-        extern nativeint SelectObject(nativeint hdc,nativeint hFont)
-
-        [<DllImport("gdi32.dll")>]
-        extern int GetCharABCWidthsFloatW (nativeint hdc, uint32 first, uint32 last, ABC* pxBuffer)
-
-        [<DllImport("gdi32.dll")>]
-        extern int GetCharWidthFloatW (nativeint hdc, uint32 first, uint32 last, float32* pxBuffer)
-
-    let getKerningPairs (g : Graphics) (f : Font) =
-        let hdc = g.GetHdc()
-        let hFont = f.ToHfont()
-        let old = SelectObject(hdc, hFont)
-        try
-            let cnt = GetKerningPairsW(hdc, 0, NativePtr.zero)
-            let ptr = NativePtr.stackalloc cnt
-            GetKerningPairsW(hdc, cnt, ptr) |> ignore
-
-            let res = Dictionary<char * char, float>()
-            for i in 0..cnt-1 do
-                let pair = NativePtr.get ptr i
-                let c0 = pair.first |> char
-                let c1 = pair.second |> char
-                res.[(c0,c1)] <- 0.25 * float pair.amount / float f.Size
-
-            res
-        finally
-            SelectObject(hdc, old) |> ignore
-            g.ReleaseHdc(hdc)
-
-    let getCharWidths (g : Graphics) (f : Font) (c : char) =
-        let hdc = g.GetHdc()
-
-        let old = SelectObject(hdc, f.ToHfont())
-        try
-
-            let index = uint32 c
-            let mutable size = ABC()
-
-            if GetCharABCWidthsFloatW(hdc, index, index, &&size) <> 0 then
-                0.75 * V3d(size.A, size.B, size.C) / float f.Size
-            else
-                let mutable size = 0.0f
-                if GetCharWidthFloatW(hdc, index, index, &&size) <> 0 then
-                    0.75 * V3d(0.0, float size, 0.0) / float f.Size
-                else
-                    Log.warn "no width for glyph '%c' (%d)" c (int c)
-                    V3d.Zero
-        finally
-            SelectObject(hdc, old) |> ignore
-            g.ReleaseHdc(hdc)
-
-module FontInfo =
-    let getKerningPairs (g : Graphics) (f : Font) =
-        match Environment.OSVersion with
-            | Windows -> GDI32.getKerningPairs g f
-            | Linux -> failwithf "[Font] implement kerning for Linux"
-            | Mac -> failwithf "[Font] implement kerning for Mac OS"
-
-    let getCharWidths (g : Graphics) (f : Font) (c : char) =
-        match Environment.OSVersion with
-            | Windows -> GDI32.getCharWidths g f c
-            | Linux -> failwithf "[Font] implement character sizes for Linux"
-            | Mac -> failwithf "[Font] implement character sizes for Mac OS"
-
 
 type Shape(path : Path) =
 
@@ -141,146 +45,195 @@ type Shape(path : Path) =
                 geometry <- Some g
                 g
 
-type Glyph internal(path : Path, graphics : Graphics, font : System.Drawing.Font, c : char) =
-    inherit Shape(path)
-    let widths = lazy (FontInfo.getCharWidths graphics font c)
+open Typography.OpenFont
 
+[<AutoOpen>]
+module private Typography =
+
+    module GlyphPoint =
+        let toV2d (scale : float) (p : GlyphPointF) = scale * V2d(p.X, p.Y)
+
+    module Bounds =
+        let toBox2d (scale : float) (b : Bounds) =
+            Box2d(scale * float b.XMin, scale * float b.YMin, scale * float b.XMax, scale * float b.YMax)
+
+    module Path =
+        type PathTranslator(scale : float) =
+            let mutable start : Option<V2d> = None
+            let mutable pos : Option<V2d> = None
+            let list = System.Collections.Generic.List<PathSegment>()
+
+            member x.ToArray() = list.ToArray()
+
+            interface IGlyphTranslator with
+                member x.BeginRead(_) = ()
+                member x.EndRead() = ()
+
+                member x.CloseContour() = 
+                    match start, pos with
+                        | Some s, Some p ->
+                            if p <> s then
+                                list.Add(PathSegment.line p s)
+                        | _ ->
+                            ()
+                    start <- None
+                    pos <- None
+                    ()
+
+                member x.MoveTo(xa,ya) =
+                    let pa = V2d(xa, ya) * scale
+                    pos <- Some pa
+                    match start with
+                        | None -> start <- Some pa
+                        | _ -> ()
+
+                member x.LineTo(xa, ya) = 
+                    match pos with
+                        | Some p0 ->
+                            let pa = V2d(xa, ya) * scale
+                            list.Add(PathSegment.line p0 pa)
+                            pos <- Some pa
+                        | None ->
+                            failwith "asdasd"
+                    ()
+                member x.Curve3(xa, ya, xb, yb) =
+                    match pos with
+                        | Some p0 ->
+                            let pa = V2d(xa, ya) * scale
+                            let pb = V2d(xb, yb) * scale
+                            
+                            list.Add(PathSegment.bezier2 p0 pa pb)
+                            pos <- Some pb
+                        | None ->
+                            failwith "asdasd"
+                    
+                member x.Curve4(xa, ya, xb, yb, xc, yc) = 
+                    match pos with
+                        | Some p0 ->
+                            let pa = V2d(xa, ya) * scale
+                            let pb = V2d(xb, yb) * scale
+                            let pc = V2d(xc, yc) * scale
+                            
+                            list.Add(PathSegment.bezier3 p0 pa pb pc)
+                            pos <- Some pc
+                        | None ->
+                            failwith "asdasd"
+
+        let ofGlyph (scale : float) (g : Glyph) =
+
+            let tx = PathTranslator(scale)
+            tx.Read(g.GlyphPoints, g.EndPoints)
+            let segments = tx.ToArray()
+                //[|
+                //    let mutable i = 0
+                //    while i < g.GlyphPoints.Length do
+                //        let mutable o = i + 1
+                //        while o < g.GlyphPoints.Length && not g.GlyphPoints.[o].OnCurve do
+                //            o <- o + 1
+                        
+                //        if o < g.GlyphPoints.Length then
+                //            let len = 1 + o - i
+                //            match len with
+                //                | 2 -> 
+                //                    let p0  = g.GlyphPoints.[i] |> GlyphPoint.toV2d scale
+                //                    let p1  = g.GlyphPoints.[o] |> GlyphPoint.toV2d scale
+                //                    yield PathSegment.line p0 p1
+                //                | 3 -> 
+                //                    let p0  = g.GlyphPoints.[i] |> GlyphPoint.toV2d scale
+                //                    let a   = g.GlyphPoints.[i+1] |> GlyphPoint.toV2d scale
+                //                    let p1  = g.GlyphPoints.[o] |> GlyphPoint.toV2d scale
+                //                    yield PathSegment.bezier2 p0 a p1
+                //                | 4 -> 
+                //                    let p0  = g.GlyphPoints.[i] |> GlyphPoint.toV2d scale
+                //                    let a   = g.GlyphPoints.[i+1] |> GlyphPoint.toV2d scale
+                //                    let b   = g.GlyphPoints.[i+2] |> GlyphPoint.toV2d scale
+                //                    let p1  = g.GlyphPoints.[o] |> GlyphPoint.toV2d scale
+                //                    yield PathSegment.bezier3 p0 a b p1
+
+                //                | _ ->
+                //                    failwithf "invalid path segment: %A" len
+
+                //        i <- o 
+                //|]
+                
+            { bounds = Bounds.toBox2d scale g.Bounds; outline = segments }
+            
+    
+
+type Glyph internal(g : Typography.OpenFont.Glyph, scale : float, advance : float, bearing : float, c : char) =
+    inherit Shape(Path.ofGlyph scale g)
+
+    let widths = 
+        let width = float (g.MaxX - g.MinX) * scale
+        V3d(bearing, width, advance - width - bearing)
+        
     member x.Character = c
     
-    member x.Bounds = path.bounds
+    member x.Bounds = base.Path.bounds
 
-    member x.Path = path
+    member x.Path = base.Path
     member x.Advance = 
-        let sizes = widths.Value
+        let sizes = widths
         sizes.X + sizes.Y + sizes.Z
 
     member x.Before = -0.1666
 
-type private FontImpl(f : System.Drawing.Font) =
-    let largeScaleFont = new System.Drawing.Font(f.FontFamily, 2000.0f, f.Style, f.Unit, f.GdiCharSet, f.GdiVerticalFont)
-    let graphics = Graphics.FromHwnd(0n, PageUnit = f.Unit)
+type private FontImpl(file : string) =
+    let f =
+        use stream = System.IO.File.OpenRead file
+        let reader = OpenFontReader()
+        reader.Read(stream, ReadFlags.Full)
 
-    let kerningTable = FontInfo.getKerningPairs graphics largeScaleFont
-
-    //let lineHeight = 1.2
-
-    let getPath (c : char) =
-        use path = new GraphicsPath()
-        let size = 1.0f
-        use fmt = new System.Drawing.StringFormat()
-        fmt.Trimming <- StringTrimming.None
-        fmt.FormatFlags <- StringFormatFlags.FitBlackBox ||| StringFormatFlags.NoClip ||| StringFormatFlags.NoWrap 
-        path.AddString(String(c, 1), f.FontFamily, int f.Style, size, PointF(0.0f, 0.0f), fmt)
-        
-
-        if path.PointCount = 0 then
-            { bounds = Box2d.Invalid; outline = [||] }
-        else
-            // build the interior polygon and boundary triangles using the 
-            // given GraphicsPath
-            let types = path.PathTypes
-            let points = path.PathPoints |> Array.map (fun p -> V2d(p.X, 1.0f - p.Y))
-
-            let mutable start = V2d.NaN
-            let currentPoints = List<V2d>()
-            let segment = List<PathSegment>()
-            let components = List<PathSegment[]>()
-
-            for (p, t) in Array.zip points types do
-                let t = t |> int |> unbox<PathPointType>
-
-                let close = t &&& PathPointType.CloseSubpath <> PathPointType.Start
-
-
-                match t &&& PathPointType.PathTypeMask with
-                    | PathPointType.Line ->
-                        if currentPoints.Count > 0 then
-                            let last = currentPoints.[currentPoints.Count - 1]
-                            match PathSegment.tryLine last p with
-                                | Some s -> segment.Add s
-                                | _ -> ()
-                            currentPoints.Clear()
-                        currentPoints.Add p
-                        
-
-
-                    | PathPointType.Bezier ->
-                        currentPoints.Add p
-                        if currentPoints.Count >= 4 then
-                            let p0 = currentPoints.[0]
-                            let p1 = currentPoints.[1]
-                            let p2 = currentPoints.[2]
-                            let p3 = currentPoints.[3]
-                            match PathSegment.tryBezier3 p0 p1 p2 p3 with
-                                | Some s -> segment.Add(s)
-                                | None -> ()
-                            currentPoints.Clear()
-                            currentPoints.Add p3
-
-                    | PathPointType.Start | _ ->
-                        currentPoints.Add p
-                        start <- p
-                        ()
-
-                if close then
-                    if not start.IsNaN && p <> start then
-                        segment.Add(Line(p, start))
-                    components.Add(CSharpList.toArray segment)
-                    segment.Clear()
-                    currentPoints.Clear()
-                    start <- V2d.NaN
-
-            let bounds = components |> Seq.collect (Seq.map PathSegment.bounds) |> Box2d
-
-
-
-
-            CSharpList.toArray components |> Array.concat |> Path.ofArray
+    let scale = f.CalculateScaleToPixel 1.0f |> float
 
     let glyphCache = Dict<char, Glyph>()
     
-    let lineHeight =
-        let s1 = graphics.MeasureString("%\r\n%", largeScaleFont)
-        let s2= graphics.MeasureString("%", largeScaleFont)
-        let s = (s1.Height - s2.Height) / largeScaleFont.Size
-        float s
-
-    let spacing =
-        
-        let s1 = graphics.MeasureString("% %", largeScaleFont)
-        let s2 = graphics.MeasureString("%%", largeScaleFont)
-        let s = (s1.Width - s2.Width) / largeScaleFont.Size
-        float s
+    let lineHeight = float (f.Ascender - f.Descender + f.LineGap) * scale
+    let spacing = float (f.GetAdvanceWidth(f.LookupIndex(int ' ') |> int)) * scale
 
     let get (c : char) =
         lock glyphCache (fun () ->
             glyphCache.GetOrCreate(c, fun c ->
-                let path = getPath c
-                Glyph(path, graphics, largeScaleFont, c)
+                let idx = f.LookupIndex(int c)
+                let glyph = f.Glyphs.[int idx]
+
+                let advance = 
+                    if glyph.HasOriginalAdvancedWidth then
+                        float glyph.OriginalAdvanceWidth  * scale
+                    else
+                        let a = f.GetHAdvanceWidthFromGlyphIndex(int idx)
+                        float a * scale
+                        
+                let bearing = 
+                    let a = f.GetHFrontSideBearingFromGlyphIndex(int idx)
+                    float a * scale
+                Glyph(glyph, scale, advance, bearing, c)
             )
         )
 
-    member x.Family = f.FontFamily.Name
+    member x.Family = f.Name
     member x.LineHeight = lineHeight
-    member x.Style = unbox<FontStyle> (int f.Style)
+    member x.Style = FontStyle.Regular
     member x.Spacing = spacing
 
     member x.GetGlyph(c : char) =
         get c
 
     member x.GetKerning(l : char, r : char) =
-        match kerningTable.TryGetValue ((l,r)) with
-            | (true, v) -> v
-            | _ -> 0.0
+        let l = f.LookupIndex (int l)
+        let r = f.LookupIndex (int r)
+        let d = f.GetKernDistance(l, r)
+        float d * scale
 
 type Font(family : string, style : FontStyle) =
-    static let table = System.Collections.Concurrent.ConcurrentDictionary<string * FontStyle, FontImpl>()
+    let impl = FontImpl(@"C:\windows\fonts\times.ttf")
+    //static let table = System.Collections.Concurrent.ConcurrentDictionary<string * FontStyle, FontImpl>()
 
-    let impl =
-        table.GetOrAdd((family, style), fun (family, style) ->
-            let f = new System.Drawing.Font(family, 1.0f, unbox (int style), GraphicsUnit.Point)
-            new FontImpl(f)
-        )
+    //let impl =
+    //    table.GetOrAdd((family, style), fun (family, style) ->
+    //        let f = new System.Drawing.Font(family, 1.0f, unbox (int style), GraphicsUnit.Point)
+    //        new FontImpl(f)
+    //    )
 
     member x.Family = family
     member x.LineHeight = impl.LineHeight
