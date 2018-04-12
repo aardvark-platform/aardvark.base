d6615b42b846c124e413ac9021a90548caff4166
Stefan Maierhofer <sm@stefanmaierhofer.com>
2018-04-12 11:28:07 +0200

    re-add TrySelect extension method

---
+++ src/Aardvark.Base/Extensions/FuncActionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Aardvark.Base
+{
+    public static partial class FuncActionExtensions
+    {
+        /// <summary>
+         /// Encapsulates the expression "[object] != null ? 'select something from [object]' : defaultValue
+         /// </summary>
+        public static Tr TrySelect<To, Tr>(this To o, Func<To, Tr> selector, Tr defaultValue = default(Tr))
+            where To : class
+        {
+            return o != null ? selector(o) : defaultValue;
+        }
+    }
+}
