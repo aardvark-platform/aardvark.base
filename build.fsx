2385c3959f569cff0aaf3cc15fedcb0e86c6e0b4
krauthaufen <krauthaufen@awx.at>
2019-07-25 09:27:30 +0200

    prettify build.fsx

--- build.fsx
+++ build.fsx
@@ -1,4 +1,3 @@
-
 #r "paket: groupref Build //"
 #load ".fake/build.fsx/intellisense.fsx"
 #load @"paket-files/build/aardvark-platform/aardvark.fake/DefaultSetup.fsx"
@@ -8,7 +7,6 @@ open System.IO
 open System.Diagnostics
 open Aardvark.Fake
 
-//do MSBuildDefaults <- { MSBuildDefaults with Verbosity = Some Minimal }
 do Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
 DefaultSetup.install ["src/Aardvark.sln"]
