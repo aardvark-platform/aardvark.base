5d092388e642ddf116f6c9de3c27232ea54142bb
Christian Luksch <luksch@vrvis.at>
2019-11-21 15:58:57 +0100

    [Test] added option to prevent Microsoft.NET.Test.Sdk to auto-generate a Main so we can have our own (command line "dotnet test" still works);

---
+++ src/Tests/Aardvark.Base.Tests/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aardvark.Tests
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            new DictTests().Run();
+        }
+    }
+}
