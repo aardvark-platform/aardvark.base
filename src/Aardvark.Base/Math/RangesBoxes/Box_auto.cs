6ff8cf9a114e6f40bd37028ae1f73b272d02ce61
Stefan Maierhofer <sm@stefanmaierhofer.com>
2020-01-19 08:37:16 +0100

    [fix] box constructor from array of points no longer crashes on empty array, or null array
    behavior is now identical to constructor from enumerable of points (invalid box on null or empty sequence)

--- src/Aardvark.Base/Math/RangesBoxes/Box_auto.cs
+++ src/Aardvark.Base/Math/RangesBoxes/Box_auto.cs
@@ -7295,9 +7295,11 @@ namespace Aardvark.Base
         /// </summary>
         public Box2i(V2i[] points)
         {
-            Min = Max = points[0];
-            long count = points.LongLength;
-            for (long i = 1; i < count; i++) ExtendBy(points[i]);
+            Min = V2i.MaxValue;
+            Max = V2i.MinValue;
+            if (points == null) return;
+            var count = points.Length;
+            for (var i = 0; i < count; i++) ExtendBy(points[i]);
         }
 
         /// <summary>
@@ -8575,9 +8577,11 @@ namespace Aardvark.Base
         /// </summary>
         public Box2l(V2l[] points)
         {
-            Min = Max = points[0];
-            long count = points.LongLength;
-            for (long i = 1; i < count; i++) ExtendBy(points[i]);
+            Min = V2l.MaxValue;
+            Max = V2l.MinValue;
+            if (points == null) return;
+            var count = points.Length;
+            for (var i = 0; i < count; i++) ExtendBy(points[i]);
         }
 
         /// <summary>
@@ -9855,9 +9859,11 @@ namespace Aardvark.Base
         /// </summary>
         public Box2f(V2f[] points)
         {
-            Min = Max = points[0];
-            long count = points.LongLength;
-            for (long i = 1; i < count; i++) ExtendBy(points[i]);
+            Min = V2f.MaxValue;
+            Max = V2f.MinValue;
+            if (points == null) return;
+            var count = points.Length;
+            for (var i = 0; i < count; i++) ExtendBy(points[i]);
         }
 
         /// <summary>
@@ -11231,9 +11237,11 @@ namespace Aardvark.Base
         /// </summary>
         public Box2d(V2d[] points)
         {
-            Min = Max = points[0];
-            long count = points.LongLength;
-            for (long i = 1; i < count; i++) ExtendBy(points[i]);
+            Min = V2d.MaxValue;
+            Max = V2d.MinValue;
+            if (points == null) return;
+            var count = points.Length;
+            for (var i = 0; i < count; i++) ExtendBy(points[i]);
         }
 
         /// <summary>
@@ -12609,9 +12617,11 @@ namespace Aardvark.Base
         /// </summary>
         public Box3i(V3i[] points)
         {
-            Min = Max = points[0];
-            long count = points.LongLength;
-            for (long i = 1; i < count; i++) ExtendBy(points[i]);
+            Min = V3i.MaxValue;
+            Max = V3i.MinValue;
+            if (points == null) return;
+            var count = points.Length;
+            for (var i = 0; i < count; i++) ExtendBy(points[i]);
         }
 
         /// <summary>
@@ -13921,9 +13931,11 @@ namespace Aardvark.Base
         /// </summary>
         public Box3l(V3l[] points)
         {
-            Min = Max = points[0];
-            long count = points.LongLength;
-            for (long i = 1; i < count; i++) ExtendBy(points[i]);
+            Min = V3l.MaxValue;
+            Max = V3l.MinValue;
+            if (points == null) return;
+            var count = points.Length;
+            for (var i = 0; i < count; i++) ExtendBy(points[i]);
         }
 
         /// <summary>
@@ -15233,9 +15245,11 @@ namespace Aardvark.Base
         /// </summary>
         public Box3f(V3f[] points)
         {
-            Min = Max = points[0];
-            long count = points.LongLength;
-            for (long i = 1; i < count; i++) ExtendBy(points[i]);
+            Min = V3f.MaxValue;
+            Max = V3f.MinValue;
+            if (points == null) return;
+            var count = points.Length;
+            for (var i = 0; i < count; i++) ExtendBy(points[i]);
         }
 
         /// <summary>
@@ -16646,9 +16660,11 @@ namespace Aardvark.Base
         /// </summary>
         public Box3d(V3d[] points)
         {
-            Min = Max = points[0];
-            long count = points.LongLength;
-            for (long i = 1; i < count; i++) ExtendBy(points[i]);
+            Min = V3d.MaxValue;
+            Max = V3d.MinValue;
+            if (points == null) return;
+            var count = points.Length;
+            for (var i = 0; i < count; i++) ExtendBy(points[i]);
         }
 
         /// <summary>
