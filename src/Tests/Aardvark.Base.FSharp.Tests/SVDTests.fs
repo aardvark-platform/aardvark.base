fbb32cbeaac6b7fe1141f11ada2b658f5f506c83
Christian Luksch <luksch@vrvis.at>
2020-01-31 19:55:59 +0100

    added SVD tests;

---
+++ src/Tests/Aardvark.Base.FSharp.Tests/SVDTests.fs
@@ -0,0 +1,45 @@
+﻿namespace Aardvark.Base.FSharp.Tests
+
+open System
+open Aardvark.Base
+open FsUnit
+open NUnit.Framework
+open System.Runtime.InteropServices
+open System.Diagnostics
+open System.Collections.Generic
+
+
+
+module SVDTests =
+    
+
+    [<Test>]
+    let ``[SVD] not 0``() =
+        for i in 0..100000 do
+            let mat = new M33d(100.0 + (float)i*1e-9, 0.0, 0.0,
+                                 0.0, 0.998672148110852, 0.051516410857669677,
+                                 0.0, -0.051516410857669677, 0.998672148110852)
+
+            let svd = SVD.decompose mat
+
+            match svd with
+            | Some (q, s, v) -> if q = M33d.Zero then
+                                    failwithf "ALL 0 at %d" i
+            | _ -> failwith "NONE"
+
+
+    [<Test>]
+    let ``[SVD] random``() =
+
+        let rnd = new RandomSystem(1)
+        for i in 0..100000 do
+            let mat = M33d.FromCols(rnd.UniformV3d(), rnd.UniformV3d(), rnd.UniformV3d())
+
+            let svd = SVD.decompose mat
+
+            match svd with
+            | Some (q, s, v) -> let test = q * s * v
+                                if not (Fun.ApproximateEquals(test, mat, 1e-6)) then
+                                    failwithf "Invalid SVD at %d" i
+            | _ -> failwith "NONE"
+
