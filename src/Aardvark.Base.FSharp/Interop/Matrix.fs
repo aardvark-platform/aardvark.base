268c955fd84993dc11b54571831f9676fcb980d1
Martin <mautner@vrvis.at>
2020-03-24 17:59:55 +0100

    Mat.transform works with non-square matrices now

--- src/Aardvark.Base.FSharp/Interop/Matrix.fs
+++ src/Aardvark.Base.FSharp/Interop/Matrix.fs
@@ -24,7 +24,7 @@ module Mat =
     let inline inverse (m : ^a) : ^b =
         (^a : (member Inverse : ^b) m)
         
-    let inline transform (m : ^a) (v : ^b) : ^b =
+    let inline transform (m : ^a) (v : ^b) =
         transformAux Unchecked.defaultof<Mat> m v
 
     let inline transformPos (m : ^a) (v : ^b) : ^b =
@@ -35,6 +35,29 @@ module Mat =
 
     let inline transformPosProj (m : ^a) (v : ^b) : ^b =
         transformPosProjAux Unchecked.defaultof<Mat> m v
+
+    module private CompilerTests =
+
+        let transformWorking () =
+            let a : V4d = transform M44d.Identity V4d.Zero
+            let a : V3d = transform M34d.Zero V4d.Zero
+            let a = (transform M34d.Zero V4d.Zero) * 0.5
+            let a = log (0.5 * (transform M22d.Identity V2d.Zero))
+
+            let a : V3d = transformPos M44d.Identity V3d.Zero
+            let a : V3d = transformPos M34d.Zero V3d.Zero
+            let a = (transformPos M34d.Zero V3d.Zero) * 0.5
+            let a = log (0.5 * (transformPos M33d.Identity V2d.Zero))
+
+            let a : V3d = transformDir M44d.Identity V3d.Zero
+            let a : V3d = transformDir M34d.Zero V3d.Zero
+            let a = (transformDir M34d.Zero V3d.Zero) * 0.5
+            let a = log (0.5 * (transformDir M33d.Identity V2d.Zero))
+
+            let a : V3d = transformPosProj M44d.Identity V3d.Zero
+            let a = log (0.5 * (transformPosProj M33d.Identity V2d.Zero))
+
+            ()
 
 [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
 module Trafo =
