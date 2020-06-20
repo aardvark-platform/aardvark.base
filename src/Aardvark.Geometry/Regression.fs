0e5751bb49d99d123a1e60a024d6478df3ed2474
Georg Haaser <krauthaufen@awx.at>
2020-06-20 14:14:25 +0200

    Aardvark.Geometry: added Regression2d

--- src/Aardvark.Geometry/Regression.fs
+++ src/Aardvark.Geometry/Regression.fs
@@ -5,7 +5,11 @@ open Aardvark.Base
 open System.Collections.Generic
 
 [<AutoOpen>]
-module private Regression3dHelpers =
+module private RegressionHelpers =
+
+    let inline sortTupleAbs (struct(a,b)) =
+        if abs a < abs b then struct(a, b)
+        else struct(b, a)
 
     let inline sortTripleAbs (struct(a,b,c)) =
         let aa = abs a
@@ -21,6 +25,347 @@ module private Regression3dHelpers =
             if aa < cc then struct(b,a,c)
             elif bb < cc then struct(b,c,a)
             else struct(c,b,a)
+
+
+
+[<Struct>]
+type Regression2d =
+    
+    val mutable private _reference : V2d
+    val mutable private _sum : V2d
+    val mutable private _sumSq : V2d
+    val mutable private _off : float
+    val mutable private _count : int
+    
+    private new(reference : V2d, sum : V2d, sumSq : V2d, off : float, count : int) =
+        { _reference = reference; _sum = sum; _sumSq = sumSq; _off = off; _count = count }
+        
+    /// Creates a new regression with a single point.
+    new(point : V2d) =
+        { _reference = point; _sum = V2d.Zero; _sumSq = V2d.Zero; _off = 0.0; _count = 1 }
+        
+    /// Creates a new regression from the given points.
+    new([<ParamArray>] points : V2d[]) =
+        if points.Length = 0 then
+            { _reference = V2d.Zero; _sum = V2d.Zero; _sumSq = V2d.Zero; _off = 0.0; _count = 0 }
+        else
+            let reference = points.[0]
+            let mutable sum = V2d.Zero
+            let mutable sumSq = V2d.Zero
+            let mutable off = 0.0
+            let mutable count = 1
+
+            for i in 1 .. points.Length - 1 do
+                let pt = points.[i] - reference
+                sum <- sum + pt
+                sumSq <- sumSq + sqr pt
+                off <- off + pt.X*pt.Y
+                count <- count + 1
+            
+            { _reference = reference; _sum = sum; _sumSq = sumSq; _off = off; _count = count }
+         
+    /// Creates a new regression from the given points.
+    new(s : seq<V2d>) = Regression2d(Seq.toArray s)
+    
+    /// Creates a new regression from the given points.
+    new(s : list<V2d>) = Regression2d(List.toArray s)
+    
+    /// A regression holding no points.
+    static member Zero = Regression2d(V2d.Zero, V2d.Zero, V2d.Zero, 0.0, 0)
+
+    /// A regression holding no points.
+    static member Empty = Regression2d(V2d.Zero, V2d.Zero, V2d.Zero, 0.0, 0)
+   
+    /// The total number of points added. Note that at least 3 points are required to fit a plane.
+    member x.Count = x._count
+
+    member private x.WithReferencePoint(r : V2d) =
+        let d = x._reference - r
+        if r + d = r then
+            x
+        else
+            let n = float x._count
+
+            // sum ((xi+dx)*(yi+dy))
+            // sum (xi*yi + dx*yi + dy*xi + dx*dy)
+            // sum (xi*yi) + dx*sum(yi) + dy*sum(xi) + n*dx*dy
+            let vxy = d.X*x._sum.Y + d.Y*x._sum.X + n*d.X*d.Y
+
+            // sum (xi+d)^2 
+            // sum (xi^2 + 2xi*d + d^2)
+            // sum (xi^2) + 2*d*sum(xi) + n*d^2
+            Regression2d(
+                r, 
+                x._sum + n*d,
+                x._sumSq + 2.0*d*x._sum + n*sqr d,
+                x._off + vxy,
+                x._count
+            )
+
+    /// The centroid of all added points.
+    member x.Centroid = 
+        if x._count = 0 then V2d.Zero
+        else x._reference + x._sum / float x._count
+
+
+    /// Adds the given point to the regression.
+    member x.Add(pt : V2d) =
+        if x._count = 0 then
+            Regression2d(pt, V2d.Zero, V2d.Zero, 0.0, 1)
+        else
+            let pt = pt - x._reference
+            Regression2d(
+                x._reference,
+                x._sum + pt,
+                x._sumSq + sqr pt,
+                x._off + pt.X*pt.Y,
+                x._count + 1
+            )
+
+    /// Adds the given point to the regression (mutating the regression).
+    member x.AddInPlace(pt : V2d) =
+        if x._count = 0 then
+            x._reference <- pt
+            x._sum <- V2d.Zero
+            x._sumSq <- V2d.Zero
+            x._off <- 0.0
+            x._count <- 1
+        else
+            let pt = pt - x._reference
+            x._sum <- x._sum + pt
+            x._sumSq <- x._sumSq + sqr pt
+            x._off <- x._off + pt.X*pt.Y
+            x._count <- x._count + 1
+    
+            
+    /// Removes the given point from the regression assuming that it has previously been added.
+    /// NOTE: when removing non-added points the regression will produce inconsistent results.
+    member x.Remove(pt : V2d) =
+        if x._count <= 1 then
+            Regression2d.Empty
+        else
+            let pt = pt - x._reference
+            Regression2d(
+                x._reference,
+                x._sum - pt,
+                x._sumSq - sqr pt |> max V2d.Zero,
+                x._off - pt.X*pt.Y,
+                x._count - 1
+            )
+ 
+    /// Removes the given point from the regression assuming that it has previously been added.
+    /// NOTE: when removing non-added points the regression will produce inconsistent results.
+    member x.RemoveInPlace(pt : V2d) =
+        if x._count <= 1 then
+            x._reference <- V2d.Zero
+            x._sum <- V2d.Zero
+            x._sumSq <- V2d.Zero
+            x._off <- 0.0
+            x._count <- 0
+        else
+            let pt = pt - x._reference
+            x._sum <- x._sum - pt
+            x._sumSq <- x._sumSq - sqr pt
+            x._off <- x._off - pt.X*pt.Y
+            x._count <- x._count - 1
+
+ 
+    /// Gets the variances as [Var(X), Var(Y), Var(Z)]
+    member x.Variance =
+        if x._count < 2 then
+            V2d.Zero
+        else
+            let n = float x._count
+            let avg = x._sum / n
+            let xx = (x._sumSq.X - avg.X * x._sum.X) / (n - 1.0)
+            let yy = (x._sumSq.Y - avg.Y * x._sum.Y) / (n - 1.0)
+            V2d(xx, yy)
+
+    /// Gets the covariance.
+    member x.Covariance =
+        if x._count < 2 then
+            0.0
+        else
+            let n = float x._count
+            let avg = x._sum / n
+            let xy = (x._off - avg.X * x._sum.Y) / (n - 1.0)
+            xy
+ 
+
+    /// Gets the least-squares plane and a confidence value in [0,1] where 1 stands for a perfect plane.
+    member x.GetPlaneAndConfidence() =
+        if x._count < 2 then
+            struct(Plane2d.Invalid, 0.0)
+        else
+            let n = float x._count
+            let avg = x._sum / n
+            let xx = (x._sumSq.X - avg.X * x._sum.X) / (n - 1.0)
+            let yy = (x._sumSq.Y - avg.Y * x._sum.Y) / (n - 1.0)
+
+            let xy = (x._off - avg.X * x._sum.Y) / (n - 1.0)
+            
+            // (xx-l)*(yy-l) - xy^2 = 0
+            // l^2 - l*(xx+yy) + xx*yy - xy^2 = 0
+
+
+            let _a = 1.0
+            let b = xx + yy
+            let c = xx*yy - sqr xy
+
+
+            let struct (l0, l1) = sortTupleAbs (Polynomial.RealRootsOfNormed(b, c))
+            if Fun.IsTiny l1 then
+                struct(Plane2d.Invalid, 0.0)
+            else
+                let goodness = 1.0 - abs l0 / abs l1
+                
+                if goodness > 1E-5 then
+                    let normal = V2d(xx - l0, xy) |> Vec.normalize
+                    let normal = V2d(-normal.Y, normal.X)
+                    struct(Plane2d(normal, x._reference + avg), goodness)
+                else
+                    struct(Plane2d.Invalid, goodness)
+
+    /// Gets a Trafo3d for the entire plane (using its principal components) and the respective standard deviations for each axis.
+    member x.GetTrafoAndSizes() =
+        if x._count < 2 then
+            struct(Trafo2d.Identity, V2d.Zero)
+        else
+            let n = float x._count
+            let avg = x._sum / n
+            let xx = (x._sumSq.X - avg.X * x._sum.X) / (n - 1.0)
+            let yy = (x._sumSq.Y - avg.Y * x._sum.Y) / (n - 1.0)
+
+            let xy = (x._off - avg.X * x._sum.Y) / (n - 1.0)
+            
+            let _a = 1.0
+            let b = xx + yy
+            let c = xx*yy - sqr xy
+
+            let struct (l0, l1) = sortTupleAbs (Polynomial.RealRootsOfNormed(b, c))
+            if Fun.IsTiny l1 then
+                struct (Trafo2d.Identity, x._reference + avg)
+            else
+                let vx = V2d(xx - l0, xy) |> Vec.normalize
+                let vy = V2d(-vx.Y, vx.X)
+               
+                let s0 = if l0 < 0.0 then 0.0 else sqrt l0
+                let s1 = if l1 < 0.0 then 0.0 else sqrt l1
+                let dev = V2d(s1,s0)
+
+                let trafo =
+                    let o = x._reference + avg
+                    Trafo2d(
+                        M33d(
+                            vx.X, vy.X, o.X,
+                            vx.Y, vy.Y, o.Y,
+                            0.0,  0.0,  1.0
+                        ),
+                        M33d(
+                            vx.X, vx.Y, -Vec.dot vx o,
+                            vy.X, vy.Y, -Vec.dot vy o,
+                            0.0,  0.0,   1.0
+                        )
+                    )
+                struct(trafo, dev)
+   
+    /// Gets the least-squares plane.
+    member x.GetPlane() =
+        let struct(p, _) = x.GetPlaneAndConfidence()
+        p
+        
+    /// Gets a Trafo3d for the entire plane using its principal components.
+    member x.GetTrafo() =
+        let struct(t, _) = x.GetTrafoAndSizes()
+        t
+  
+    static member (+) (l : Regression2d, r : Regression2d) =
+        if r.Count <= 0 then l
+        elif l.Count <= 0 then r
+        elif l.Count = 1 then r.Add l._reference
+        elif r.Count = 1 then l.Add r._reference
+        else
+            let r1 = r.WithReferencePoint l._reference
+            Regression2d(
+                l._reference,
+                l._sum + r1._sum,
+                l._sumSq + r1._sumSq,
+                l._off + r1._off,
+                l._count + r1._count
+            )
+        
+    static member (-) (l : Regression2d, r : Regression2d) =
+        if r.Count <= 0 then l
+        elif l.Count <= r.Count then Regression2d.Zero
+        elif r.Count = 1 then l.Remove r._reference
+        else
+            let r = r.WithReferencePoint l._reference
+            Regression2d(
+                l._reference,
+                l._sum - r._sum,
+                l._sumSq - r._sumSq,
+                l._off - r._off,
+                l._count - r._count
+            )
+        
+    static member (+) (l : Regression2d, r : V2d) =
+        l.Add r
+        
+    static member (+) (l : V2d, r : Regression2d) =
+        r.Add l
+        
+    static member (-) (l : Regression2d, r : V2d) =
+        l.Remove r
+
+module Regression2d =
+
+    /// A regression holding no points.
+    let empty = Regression2d.Zero
+
+    /// Removes the given point from the regression assuming that it has previously been added.
+    /// NOTE: when removing non-added points the regression will produce inconsistent results.
+    let inline remove (pt : V2d) (s : Regression2d) = s.Remove pt
+
+    /// Adds the given point to the regression.
+    let inline add (pt : V2d) (s : Regression2d) = s.Add pt
+
+    /// Gets the least-squares plane.
+    let inline getPlane (s : Regression2d) = s.GetPlane()
+
+    /// Gets a Trafo2d for the entire plane using its principal components.
+    let inline getTrafo (s : Regression2d) = s.GetTrafo()
+    
+    /// The total number of points added. Note that at least 2 points are required to fit a line.
+    let inline count (s : Regression2d) = s.Count
+    
+    /// The centroid of all added points.
+    let inline centroid (s : Regression2d) = s.Centroid
+    
+    /// Gets the variances as [Var(X), Var(Y)]
+    let inline variance (s : Regression2d) = s.Variance
+
+    /// Gets the covariance.
+    let inline covariance (s : Regression2d) = s.Covariance
+
+    /// Creates a regression from the given points.
+    let ofSeq (points : seq<V2d>) =
+        let mutable r = empty
+        for p in points do r.AddInPlace p
+        r
+        
+    /// Creates a regression from the given points.
+    let ofList (points : list<V2d>) =
+        let mutable r = empty
+        for p in points do r.AddInPlace p
+        r
+        
+    /// Creates a regression from the given points.
+    let ofArray (points : V2d[]) =
+        let mutable r = empty
+        for p in points do r.AddInPlace p
+        r
+
+
 
 [<Struct>]
 type Regression3d =
@@ -73,11 +418,6 @@ type Regression3d =
     
     /// The total number of points added. Note that at least 3 points are required to fit a plane.
     member x.Count = x._count
-
-    member private x.ReferencePoint = x._reference
-    member private x.Sum = x._sum
-    member private x.SumSq = x._sum
-    member private x.OffDiag = x._off
 
     member private x.WithReferencePoint(r : V3d) =
         let d = x._reference - r
@@ -350,10 +690,10 @@ type Regression3d =
     static member (+) (l : Regression3d, r : Regression3d) =
         if r.Count <= 0 then l
         elif l.Count <= 0 then r
-        elif l.Count = 1 then r.Add l.ReferencePoint
-        elif r.Count = 1 then l.Add r.ReferencePoint
+        elif l.Count = 1 then r.Add l._reference
+        elif r.Count = 1 then l.Add r._reference
         else
-            let r1 = r.WithReferencePoint l.ReferencePoint
+            let r1 = r.WithReferencePoint l._reference
             Regression3d(
                 l._reference,
                 l._sum + r1._sum,
@@ -365,15 +705,15 @@ type Regression3d =
     static member (-) (l : Regression3d, r : Regression3d) =
         if r.Count <= 0 then l
         elif l.Count <= r.Count then Regression3d.Zero
-        elif r.Count = 1 then l.Remove r.ReferencePoint
+        elif r.Count = 1 then l.Remove r._reference
         else
-            let r = r.WithReferencePoint l.ReferencePoint
+            let r = r.WithReferencePoint l._reference
             Regression3d(
-                l.ReferencePoint,
-                l.Sum - r.Sum,
-                l.SumSq - r.SumSq,
-                l.OffDiag - r.OffDiag,
-                l.Count - r.Count
+                l._reference,
+                l._sum - r._sum,
+                l._sumSq - r._sumSq,
+                l._off - r._off,
+                l._count - r._count
             )
         
     static member (+) (l : Regression3d, r : V3d) =
--- src/Tests/Aardvark.Geometry.Tests/Regression.fs
+++ src/Tests/Aardvark.Geometry.Tests/Regression.fs
@@ -12,10 +12,23 @@ open Aardvark.Geometry
 module Regression3dTests =
     
     module Gen =
-        let uniform (min : float) (max : float) = 
-            Gen.choose (0, Int32.MaxValue) 
-            |> Gen.map (fun c -> min + (max - min) * (float c / float Int32.MaxValue))
 
+        /// generates a float in [0,1]
+        let uniformFloat =
+            // choose two ints with 22/31 bits, create an uint64 from these and normalize to float.
+            // this creates a full-precision float since float has 53 "effective" significand bits.
+            (Gen.choose (0, 4194303), Gen.choose (0, Int32.MaxValue)) ||> Gen.map2 (fun a b ->
+                let v = (uint64 a <<< 31) ||| uint64 b
+                float v / 9007199254740991.0
+            )                
+
+        /// generates a float in [min, max]
+        let uniform (min : float) (max : float) =
+            uniformFloat |> Gen.map (fun v ->
+                min + v * (max - min)
+            )
+
+        /// generates a float from N(avg, dev)
         let gauss (avg : float) (dev : float) =
             uniform -1.0 1.0 |> Gen.map (fun v ->
                 let side = sign v
@@ -25,6 +38,7 @@ module Regression3dTests =
                 n01 * dev + avg
             )
 
+        /// generates a normalized V3d direction
         let direction =
             gen {
                 let! phi = uniform 0.0 Constant.PiTimesTwo
@@ -45,7 +59,19 @@ module Regression3dTests =
         override x.ToString() =
             sprintf "%d: %A" value.Length value
 
+    type ZeroOneFloat =
+        ZeroOneFloat of float
+    type GaussFloat =
+        GaussFloat of float
+
+
     type EuclideanGenerator private() =
+
+        static member ZeroOneFloat =
+            Gen.uniformFloat |> Gen.map ZeroOneFloat |> Arb.fromGen
+
+        static member GaussFloat =
+            Gen.gauss 0.0 1.0 |> Gen.map GaussFloat |> Arb.fromGen
 
         static member AtLeast<'d, 'a when 'd :> INatural>() =
             gen {
@@ -80,7 +106,16 @@ module Regression3dTests =
                 let! y = Gen.gauss 0.0 100.0
                 return V2d(x,y)
             } |> Arb.fromGen
+            
+        static member Euclidean2d =
+            gen {
+                let! x = Gen.gauss 0.0 300.0
+                let! y = Gen.gauss 0.0 300.0
+                let! angle = Gen.uniform 0.0 Constant.PiTimesTwo
 
+                return Euclidean2d(Rot2d angle, V2d(x,y))
+
+            } |> Arb.fromGen
         static member Euclidean3d =
             gen {
                 let! x = Gen.gauss 0.0 300.0
@@ -104,10 +139,24 @@ module Regression3dTests =
         let trafo = r.GetTrafo()
         let error = points |> Array.map (fun p -> trafo.Backward.TransformPos(p).Z |> abs) |> Array.max
         error |> should be (lessThanOrEqualTo 1E-6)
-
+        
     let check (points : V3d[]) (r : Regression3d) =
         checkPlane r points
         checkTrafo r points
+        
+    let checkPlane2d (r : Regression2d) (points : V2d[]) =
+        let struct(plane, _) = r.GetPlaneAndConfidence()
+        let error = points |> Array.map (plane.Height >> abs) |> Array.max
+        error |> should be (lessThanOrEqualTo 1E-6)
+        
+    let checkTrafo2d (r : Regression2d) (points : V2d[]) =
+        let trafo = r.GetTrafo()
+        let error = points |> Array.map (fun p -> trafo.Backward.TransformPos(p).Y |> abs) |> Array.max
+        error |> should be (lessThanOrEqualTo 1E-6)
+        
+    let check2d (points : V2d[]) (r : Regression2d) =
+        checkPlane2d r points
+        checkTrafo2d r points
 
     [<Property(Arbitrary = [| typeof<EuclideanGenerator> |])>]
     let ``Regression3d plane working`` (trafo : Euclidean3d) (points : AtLeast<8 N, V2d>) =
@@ -131,3 +180,25 @@ module Regression3dTests =
 
         // remove working
         pts |> Array.fold (+) (Regression3d V3d.Zero) |> Regression3d.remove V3d.Zero |> check pts
+        
+
+    [<Property(Arbitrary = [| typeof<EuclideanGenerator> |])>]
+    let ``Regression2d plane working`` (trafo : Euclidean2d) (points : AtLeast<8 N, GaussFloat>) =
+        let pts = points.Value |> Array.map (fun (GaussFloat v) -> trafo.TransformPos(V2d(v * 10.0, 0.0)))
+        
+        // add working
+        pts |> Array.fold (+) Regression2d.empty |> check2d pts
+
+        // ofArray/ofSeq working
+        pts |> Regression2d.ofArray |> check2d pts
+        pts |> Regression2d.ofSeq |> check2d pts
+
+        // constructor working
+        pts |> Regression2d |> check2d pts
+
+        // regression addition working
+        let n2 = pts.Length / 2
+        Regression2d.ofArray pts.[..n2-1] + Regression2d.ofArray pts.[n2..] |> check2d pts
+
+        // remove working
+        pts |> Array.fold (+) (Regression2d V2d.Zero) |> Regression2d.remove V2d.Zero |> check2d pts
