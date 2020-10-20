a166008a23c191a55e57d3d436bafb7140438efc
Georg Haaser <krauthaufen@awx.at>
2020-10-20 17:11:23 +0200

    TexturePacking fixes

--- src/Tests/Aardvark.Geometry.Tests/TexturePacking.fs
+++ src/Tests/Aardvark.Geometry.Tests/TexturePacking.fs
@@ -16,7 +16,9 @@ module TexturePackingTests =
     type Size = Size of V2i
 
     type SizeGenerator() =
-        
+        static member SizeGen =
+            (Gen.choose (1, 64), Gen.choose (1,64)) ||> Gen.map2 (fun x y -> V2i(x,y) |> Size)
+
         static member Size() =
             (Gen.choose (1, 64), Gen.choose (1,64)) ||> Gen.map2 (fun x y -> V2i(x,y) |> Size) |> Arb.fromGen
 
@@ -25,6 +27,11 @@ module TexturePackingTests =
     let ``square producing correct results``(NonEmptyArray (sizes)) =
         let sizes = sizes |> Array.mapi (fun i (Size s) -> i, s)
         let packing = TexturePacking.square sizes
+
+        let mutable res = packing
+        for i,_ in sizes do
+            res <- res.Remove i
+
 
         let bb = Box2i(V2i.Zero, packing.Size - V2i.II)
 
@@ -54,57 +61,74 @@ module TexturePackingTests =
         // no extra rects
         all.IsEmpty |> should be True
 
-    [<Test>]
-    let ``occupancy``() =
-
-        let rand = RandomSystem()
-
-        for cnt in [10;50;100] do
-            let histogram = Array.zeroCreate 20
-            let binSize = 1.0 / float histogram.Length
-
-            printfn "%d:" cnt
-            let iter = 100
-            let mutable oMin = 1.0
-            let mutable oMax = 0.0
-            let mutable oSum = 0.0
-            for i in 1 .. iter do
-                let sizes = Array.init cnt (fun i -> i, rand.UniformV2i(63) + V2i.II)
-                let packing = TexturePacking.square sizes
-
-                let o = packing.Occupancy
-
-                let bin = floor (o * float histogram.Length) |> int |> clamp 0 (histogram.Length - 1)
-                histogram.[bin] <- histogram.[bin] + 1
-
-                oMin <- min oMin o
-                oMax <- max oMax o
-                oSum <- oSum + o
-
-            let oAvg = oSum / float iter
-
-            printfn "  min: %.2f%%" (100.0 * oMin)
-            printfn "  max: %.2f%%" (100.0 * oMax)
-            printfn "  avg: %.2f%%" (100.0 * oAvg)
+    [<EntryPoint>]
+    let run args =
+        
+        let gen = SizeGenerator()
+        let seed = Random.mkStdGen 239856472398472L
 
 
-            let maxHisto = Array.max histogram
-            let w = 10
-            let mutable minValue = 0.0
-            let mutable maxValue = binSize
-            let mutable started = false
-            for h in histogram do
-                let p = float h / float iter
-                if h > 0 then started <- true
+        for size in [1;2;5;10;30] do
+            let tests = Gen.eval size seed (Gen.listOfLength 1000 (Gen.nonEmptyListOf SizeGenerator.SizeGen))
 
-                if started then
-                    let barWidth = (float w * float h / float maxHisto) |> round |> int
-                    let bar = System.String('#', barWidth) + System.String(' ', w - barWidth)
+            let tests = [ List.init size (fun _ -> Size(V2i(37,37))) ] :: tests
 
-                    if maxValue >= 0.999999 then
-                        printfn "   >= %.0f%%    %s %.2f%%" (100.0 * minValue) bar (100.0 * p)
+            for t in tests do
+                ``square producing correct results`` (NonEmptyArray (List.toArray t))
+                printfn "%A" t
+        0
 
-                    else 
-                        printfn "  [%.0f%%, %.0f%%) %s %.2f%%" (100.0 * minValue) (100.0 * maxValue) bar (100.0 * p)
-                minValue <- minValue + binSize
-                maxValue <- maxValue + binSize
+    //[<Test>]
+    //let ``occupancy``() =
+
+    //    let rand = RandomSystem()
+
+    //    for cnt in [10;50;100] do
+    //        let histogram = Array.zeroCreate 20
+    //        let binSize = 1.0 / float histogram.Length
+
+    //        printfn "%d:" cnt
+    //        let iter = 100
+    //        let mutable oMin = 1.0
+    //        let mutable oMax = 0.0
+    //        let mutable oSum = 0.0
+    //        for i in 1 .. iter do
+    //            let sizes = Array.init cnt (fun i -> i, rand.UniformV2i(63) + V2i.II)
+    //            let packing = TexturePacking.square sizes
+
+    //            let o = packing.Occupancy
+
+    //            let bin = floor (o * float histogram.Length) |> int |> clamp 0 (histogram.Length - 1)
+    //            histogram.[bin] <- histogram.[bin] + 1
+
+    //            oMin <- min oMin o
+    //            oMax <- max oMax o
+    //            oSum <- oSum + o
+
+    //        let oAvg = oSum / float iter
+
+    //        printfn "  min: %.2f%%" (100.0 * oMin)
+    //        printfn "  max: %.2f%%" (100.0 * oMax)
+    //        printfn "  avg: %.2f%%" (100.0 * oAvg)
+
+
+    //        let maxHisto = Array.max histogram
+    //        let w = 10
+    //        let mutable minValue = 0.0
+    //        let mutable maxValue = binSize
+    //        let mutable started = false
+    //        for h in histogram do
+    //            let p = float h / float iter
+    //            if h > 0 then started <- true
+
+    //            if started then
+    //                let barWidth = (float w * float h / float maxHisto) |> round |> int
+    //                let bar = System.String('#', barWidth) + System.String(' ', w - barWidth)
+
+    //                if maxValue >= 0.999999 then
+    //                    printfn "   >= %.0f%%    %s %.2f%%" (100.0 * minValue) bar (100.0 * p)
+
+    //                else 
+    //                    printfn "  [%.0f%%, %.0f%%) %s %.2f%%" (100.0 * minValue) (100.0 * maxValue) bar (100.0 * p)
+    //            minValue <- minValue + binSize
+    //            maxValue <- maxValue + binSize
