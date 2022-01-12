18e0bb5a1b1ac237f02b26c5cfd604feb966f465
Harald Steinlechner <haraldsteinlechner@gmail.com>
2022-01-12 17:20:34 +0100

    added image loading tests (load/save with different loaders)

---
+++ src/Tests/Aardvark.Geometry.Tests/Program.fs
@@ -0,0 +1,11 @@
+﻿module Program
+
+open Aardvark.Geometry.Tests
+
+[<EntryPoint>]
+let main args = 
+    FsCheck.Arb.register<ImageLoadingTests.Generators>() |> ignore
+    let r = FsCheck.Check.Quick ImageLoadingTests.saveLoadTest
+    
+    //TexturePackingTests.run args
+    0
