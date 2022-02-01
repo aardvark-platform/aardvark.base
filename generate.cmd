f3bbdba9cfcb40aadba3cc3c1d52b849b7890ab7
krauthaufen <krauthaufen@awx.at>
2022-02-01 10:34:41 +0100

    another CodeGenerator test

--- generate.cmd
+++ generate.cmd
@@ -1,4 +1,5 @@
 @echo off
 dotnet build src\CodeGenerator\CodeGenerator.csproj
 dotnet build src\Aardvark.Base\Aardvark.Base.csproj
-bin\Debug\net471\CodeGenerator.exe
+pushd bin\Debug\net471\
+CodeGenerator.exe
