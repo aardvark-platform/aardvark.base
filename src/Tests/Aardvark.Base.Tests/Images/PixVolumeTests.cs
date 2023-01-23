2be4569135e2480b81bd46d3a3554e08782c378f
Martin <mautner@vrvis.at>
2023-01-23 18:37:51 +0100

    [PixVolume] Fix and improve format conversion
    See: https://github.com/aardvark-platform/aardvark.base/commit/e51dad65547fc69dbca2f566a0adc73b7a0a732e

--- src/Tests/Aardvark.Base.Tests/Images/PixVolumeTests.cs
+++ src/Tests/Aardvark.Base.Tests/Images/PixVolumeTests.cs
@@ -1,115 +1,33 @@
 ﻿using NUnit.Framework;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using Aardvark.Base;
 
 namespace Aardvark.Tests.Images
 {
     [TestFixture]
-    class PixImageTests
+    class PixVolumeTests
     {
         static readonly RandomSystem rnd = new(0);
 
-        #region Mipmap
-
-        [Test]
-        public void MipMapCreate1x1()
-        {
-            var pix = new PixImage<byte>(1, 1, 4);
-            var mip = PixImageMipMap.Create(pix);
-            Assert.IsTrue(mip.LevelCount == 1);
-        }
-
-        [Test]
-        public void MipMapCreate2x2()
-        {
-            var pix = new PixImage<byte>(2, 2, 4);
-            var mip = PixImageMipMap.Create(pix);
-            Assert.IsTrue(mip.LevelCount == 2);
-        }
-
-        [Test]
-        public void MipMapCreate3x3()
-        {
-            var pix = new PixImage<byte>(3, 3, 4);
-            var mip = PixImageMipMap.Create(pix);
-            Assert.IsTrue(mip.LevelCount == 2);
-        }
-
-        [Test]
-        public void MipMapCreate256()
-        {
-            var pix = new PixImage<byte>(256, 256, 4);
-            var mip = PixImageMipMap.Create(pix);
-            Assert.IsTrue(mip.LevelCount == 9);
-        }
-
-        [Test]
-        public void MipMapCreate255()
-        {
-            var pix = new PixImage<byte>(255, 255, 4);
-            var mip = PixImageMipMap.Create(pix);
-            Assert.IsTrue(mip.LevelCount == 8);
-        }
-
-        [Test]
-        public void MipMapCreate257()
-        {
-            var pix = new PixImage<byte>(257, 257, 4);
-            var mip = PixImageMipMap.Create(pix);
-            Assert.IsTrue(mip.LevelCount == 9);
-        }
-
-        [Test]
-        public void MipMapCreate57x43()
-        {
-            var pix = new PixImage<byte>(57, 43, 4);
-            var mip = PixImageMipMap.Create(pix);
-            Assert.IsTrue(mip.LevelCount == 6);
-            //level 0: 57x43
-            //level 1: 28x21
-            //level 2: 14x10
-            //level 3: 7x5
-            //level 4: 3x2
-            //level 5: 1x1
-        }
-
-        [Test]
-        public void MipMapCreate57x11()
-        {
-            var pix = new PixImage<byte>(57, 11, 4);
-            var mip = PixImageMipMap.Create(pix);
-            Assert.IsTrue(mip.LevelCount == 6);
-            //level 0: 57x11
-            //level 1: 28x5
-            //level 2: 14x2
-            //level 3: 7x1
-            //level 4: 3x1
-            //level 5: 1x1
-        }
-
-        #endregion
-
         #region Format Conversions
 
-        private static byte[] GetArray(PixImage<byte> pi, V2l coord)
+        private static byte[] GetArray(PixVolume<byte> pi, V3l coord)
         {
             var arr = new byte[pi.ChannelCount];
-            arr.SetByIndex(i => pi.Volume[new V3l(coord, i)]);
+            arr.SetByIndex(i => pi.Tensor4[new V4l(coord, i)]);
             return arr;
         }
 
-        private static T GetColor<T>(PixImage<byte> pi, V2l coord)
-            => pi.GetMatrix<T>()[coord];
+        private static T GetColor<T>(PixVolume<byte> pi, V3l coord)
+            => pi.GetVolume<T>()[coord];
 
         private static void FormatConversion<T1, T2>(Col.Format sourceFormat, Col.Format targetFormat,
-                                                     Func<PixImage<byte>, V2l, T1> getInput,
-                                                     Func<PixImage<byte>, V2l, T2> getActual,
+                                                     Func<PixVolume<byte>, V3l, T1> getInput,
+                                                     Func<PixVolume<byte>, V3l, T2> getActual,
                                                      Func<T1, T2> expectedConversion)
         {
-            var src = new PixImage<byte>(sourceFormat, 43, 81);
-            src.Volume.Data.SetByIndex((_) => (byte)rnd.UniformInt(256));
+            var src = new PixVolume<byte>(sourceFormat, 43, 31, 23);
+            src.Tensor4.Data.SetByIndex((_) => (byte)rnd.UniformInt(256));
 
             var dst = src.ToFormat(targetFormat);
 
