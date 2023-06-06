cbcd5cda41c5c015db4f070d857c4b07aa198d34
Martin <mautner@vrvis.at>
2023-06-06 16:50:14 +0200

    Add string extensions

--- src/Aardvark.Base.FSharp/String.fs
+++ src/Aardvark.Base.FSharp/String.fs
@@ -5,25 +5,60 @@ open Aardvark.Base
 namespace Aardvark.Base
 #endif
 
-open System.Linq.Expressions
-open System.Runtime.InteropServices
-open Microsoft.FSharp.Reflection
-open System.Reflection
-open System.Reflection.Emit
 open System
+open System.Text.RegularExpressions
+open System.Runtime.CompilerServices
 
+#if !NET6_0_OR_GREATER
+[<Sealed; Extension>]
+type StringExtensions private() =
+
+    /// Replaces all newline sequences in the current string with replacementText.
+    [<Extension>]
+    static member inline ReplaceLineEndings(str : string, replacementText : string) =
+        Regex.Replace(str, @"\r\n?|\n", replacementText)
+
+    /// Replaces all newline sequences in the current string with Environment.NewLine.
+    [<Extension>]
+    static member inline ReplaceLineEndings(str : string) =
+        str.ReplaceLineEndings(Environment.NewLine)
+#endif
 
 [<AutoOpen>]
 module ``String Extensions`` =
     module String =
-        let private lineBreak = System.Text.RegularExpressions.Regex("\r\n")
 
+        /// Splits the string into individual lines, recognizing various line ending sequences.
+        let inline getLines (str : string) =
+            str.ReplaceLineEndings().Split([| Environment.NewLine |], StringSplitOptions.None)
+
+        /// Replaces all newline sequences in the given string with Environment.NewLine.
+        let inline normalizeLineEndings (str : string) =
+            str.ReplaceLineEndings()
+
+        /// Adds line number prefixes to the given string.
+        /// Line endings are normalized.
+        let withLineNumbers (str : string) =
+            let lines = getLines str
+            let lineColumns = 1 + int (Fun.Log10 lines.Length)
+
+            lines |> Array.mapi (fun i str ->
+                let n = (string (i + 1)).PadLeft lineColumns
+                $"{n}: {str}"
+            )
+            |> String.concat Environment.NewLine
+
+        /// Indents each line of string by step * 4 spaces.
+        /// Line endings are normalized.
         let indent (step : int) (s : string) =
-            let parts = lineBreak.Split s
+            let lines = getLines s
             let indent = System.String(' ', step * 4)
-            let parts = parts |> Seq.map (fun l -> indent + l)
-            System.String.Join("\r\n", parts)
 
+            lines
+            |> Array.map (fun l -> indent + l)
+            |> String.concat Environment.NewLine
+
+        /// Gets the number of lines in the given string, recognizing various line ending sequences.
         let lineCount (s : string) =
-            let parts = lineBreak.Split s
-            parts.Length
+            let lines = getLines s
+            lines.Length
