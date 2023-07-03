ac47f83e33ac735c67e9a18db7127e020b147f78
Martin <mautner@vrvis.at>
2023-07-03 17:01:15 +0200

    Fix GetValueOrDefault dictionary extension
    Using the extension in a library targeting netstandard2.0
    and using it in an net6.0 application would lead to
    a MissingMethodException.
    
    This commit adds an alternative method that is present
    for all targets.

--- src/Aardvark.Base/Extensions/DictionaryExtensions.cs
+++ src/Aardvark.Base/Extensions/DictionaryExtensions.cs
@@ -220,11 +220,18 @@ namespace Aardvark.Base
             return true;
         }
 
-// obsolete in netcore -> replaced by System.Collections.Generic.CollectionExtensions
+        /// <summary>
+        /// Returns the value stored with the supplied key or the specified default value if not found.
+        /// </summary>
+        public static TValue GetOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> self, TKey key, TValue defaultValue)
+            => self.TryGetValue(key, out var result) ? result : defaultValue;
+
+        // obsolete in netcore -> replaced by System.Collections.Generic.CollectionExtensions
 #if !NETCOREAPP3_1_OR_GREATER
         /// <summary>
         /// Returns the value stored with the supplied key or the specified default value if not found.
         /// </summary>
+        [Obsolete("Use GetOrDefault instead.")]
         public static TValue GetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> self, TKey key, TValue default_value)
         {
             return self.TryGetValue(key, out var result) ? result : default_value;
