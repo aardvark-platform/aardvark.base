e0f644142fc9d6d24036ffa74f45e2a2a83d2189
Martin <mautner@vrvis.at>
2023-10-16 09:00:21 +0200

    [FSharp] Add more ArraySegment functions

--- src/Aardvark.Base.FSharp/Interop/ArraySegment.fs
+++ src/Aardvark.Base.FSharp/Interop/ArraySegment.fs
@@ -12,6 +12,18 @@ module ArraySegment =
             member inline x.Item(index : int) = x.Array.[x.Offset + index]
     #endif
 
+    let inline tryHeadV (segment : ArraySegment<'T>) =
+        if segment.Count > 0 then ValueSome segment.[0]
+        else ValueNone
+
+    let inline copyArray (segment : ArraySegment<'T>) =
+        let arr = Array.zeroCreate<'T> segment.Count
+        Array.Copy(segment.Array, segment.Offset, arr, 0, arr.Length)
+        arr
+
+    let inline copy (segment : ArraySegment<'T1>) =
+        ArraySegment (copyArray segment)
+
     let inline mapArray (mapping : 'T1 -> 'T2) (segment : ArraySegment<'T1>) =
         Array.init segment.Count (fun i -> mapping segment.[i])
 
@@ -28,6 +40,18 @@ module ArraySegment =
             i <- i + 1
 
         state
+
+    let inline reduce (reduction : 'T -> 'T -> 'T) (segment : ArraySegment<'T>) =
+        if segment.Count = 0 then
+            invalidArg "segment" "Cannot reduce an empty ArraySegment."
+        else
+            let f = OptimizedClosures.FSharpFunc<_, _, _>.Adapt (reduction)
+            let mutable res = segment.[0]
+
+            for i = 1 to segment.Count - 1 do
+                res <- f.Invoke(res, segment.[i])
+
+            res
 
     /// Forms a slice of the specified length out of the array segment starting at the specified index.
     let inline slice (start : int) (count : int) (segment : ArraySegment<'T>) =
