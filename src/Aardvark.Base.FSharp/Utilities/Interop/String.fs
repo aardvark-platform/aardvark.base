8a5a2e5f2c4f17e2e3903869eb643da9d9328e01
Martin <mautner@vrvis.at>
2023-10-30 12:44:35 +0100

    [FSharp] Remove Strings.fs

--- src/Aardvark.Base.FSharp/Utilities/Interop/String.fs
+++ src/Aardvark.Base.FSharp/Utilities/Interop/String.fs
@@ -9,6 +9,22 @@ open System
 open System.Text.RegularExpressions
 open System.Runtime.CompilerServices
 
+[<Obsolete("Use String module instead.")>]
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module Strings =
+    let partRx = Regex @"([A-Z][a-z0-9]*)[_]*"
+
+    /// checks whether pattern is contained in str
+    let contains pattern (str : string) = str.Contains pattern
+
+    let toLower (str : string) = str.ToLower()
+    let toUpper (str : string) = str.ToUpper()
+
+    let inline split (sep : string) (str : string) = str.Split([| sep |], StringSplitOptions.None)
+    let inline startsWith (s : string) (str : string) = str.StartsWith s
+    let inline endsWith (s : string) (str : string) = str.EndsWith s
+    let inline trim (str : string) = str.Trim()
+
 [<Sealed; Extension>]
 type StringExtensions private() =
 
