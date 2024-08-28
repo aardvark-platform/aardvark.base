f48f7ee26245ec289354ed8b2d961511a930db7e
Martin <mautner@vrvis.at>
2024-08-28 12:16:50 +0200

    Fix Stream.ReadBytes default for count parameter

--- src/Aardvark.Base/Extensions/StreamExtensions.cs
+++ src/Aardvark.Base/Extensions/StreamExtensions.cs
@@ -10,11 +10,11 @@ namespace Aardvark.Base
         /// <param name="stream">The stream to read from.</param>
         /// <param name="buffer">The buffer to read into.</param>
         /// <param name="offset">The byte offset in <paramref name="buffer"/> at which to begin storing the data read from the current stream.</param>
-        /// <param name="count">The number of bytes to be read from the current stream. Defaults to the buffer length if negative.</param>
+        /// <param name="count">The number of bytes to be read from the current stream. Defaults to <paramref name="buffer"/>.Length - <paramref name="offset"/> if negative.</param>
         /// <exception cref="EndOfStreamException"></exception>
         public static void ReadBytes(this Stream stream, byte[] buffer, int offset = 0, int count = -1)
         {
-            if (count < 0) count = buffer.Length;
+            if (count < 0) count = buffer.Length - offset;
 #if NET8_0_OR_GREATER
             stream.ReadExactly(buffer, offset, count);
 #else
